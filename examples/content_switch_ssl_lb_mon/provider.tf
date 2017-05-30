7eadc02d7732ef5e808e71793884c14d7cea4991
Chiradeep Vittal <chiradeep.vittal@citrix.com>
2017-05-30 11:49:57 -0700

    Separate out backend port in variables used in examples

--- examples/content_switch_ssl_lb_mon/provider.tf
+++ examples/content_switch_ssl_lb_mon/provider.tf
@@ -1,3 +1,3 @@
 provider "netscaler" {
-    endpoint = "http://localhost:32782/"
+    endpoint = "http://localhost:32768/"
 }
--- examples/simple_lb/resources.tf
+++ examples/simple_lb/resources.tf
@@ -11,6 +11,6 @@ resource "netscaler_servicegroup" "backend" {
   lbvserver = "${netscaler_lbvserver.production_lb.name}"
   servicetype = "HTTP"
   clttimeout = "${lookup(var.backend_service_config, "clttimeout")}"
-  servicegroupmembers = "${var.backend_services}"
+  servicegroupmembers = "${formatlist("%s:%s", var.backend_services, var.backend_service_config["backend_port"])}"
 }
 
