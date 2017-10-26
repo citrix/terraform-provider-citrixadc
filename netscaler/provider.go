141eeaf700d36e206871fd1b4c27477536635bc0
Unknown <mike.thompson@ticketmaster.com>
2017-10-26 08:36:11 -0400

    Skip SSL check
    Modified NewNitroClient to use new SSLVerify option.

--- netscaler/provider.go
+++ netscaler/provider.go
@@ -16,7 +16,7 @@ limitations under the License.
 package netscaler
 
 import (
-	"github.com/chiradeep/go-nitro/netscaler"
+	"github.com/Ticketmaster/go-nitro/netscaler"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
 )
@@ -25,6 +25,7 @@ type NetScalerNitroClient struct {
 	Username string
 	Password string
 	Endpoint string
+	SSLVerify  bool
 	client   *netscaler.NitroClient
 }
 
@@ -55,6 +56,12 @@ func providerSchema() map[string]*schema.Schema {
 			Required:    true,
 			Description: "The URL to the API",
 			DefaultFunc: schema.EnvDefaultFunc("NS_URL", nil),
+		},		
+		"sslverify": &schema.Schema{
+			Type:        schema.TypeBool,
+			Required:    true,
+			DefaultFunc: schema.EnvDefaultFunc("NS_SSLVERIFY", true),
+			Description: "Enable ssl validation",
 		},
 	}
 }
@@ -77,8 +84,9 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 		Username: d.Get("username").(string),
 		Password: d.Get("password").(string),
 		Endpoint: d.Get("endpoint").(string),
+		SSLVerify: d.Get("sslverify").(bool),
 	}
-	client := netscaler.NewNitroClient(c.Endpoint, c.Username, c.Password)
+	client := netscaler.NewNitroClient(c.Endpoint, c.Username, c.Password, c.SSLVerify)
 	c.client = client
 
 	return &c, nil
