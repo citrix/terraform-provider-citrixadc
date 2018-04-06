citrix/terraform-provider-citrixadc

commit d1b099719a6ca956f57b89619af94fcd7bb27e04

Temp Commit: ignore TLS

netscaler/provider.go
@@ -38,23 +38,29 @@ func Provider() terraform.ResourceProvider {
 
 func providerSchema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
-		"username": &schema.Schema{
+		"username": {
 			Type:        schema.TypeString,
 			Required:    true,
 			Description: "Username to login to the NetScaler",
 			DefaultFunc: schema.EnvDefaultFunc("NS_LOGIN", "nsroot"),
 		},
-		"password": &schema.Schema{
+		"password": {
 			Type:        schema.TypeString,
 			Required:    true,
 			Description: "Password to login to the NetScaler",
 			DefaultFunc: schema.EnvDefaultFunc("NS_PASSWORD", "nsroot"),
 		},
-		"endpoint": &schema.Schema{
+		"endpoint": {
 			Type:        schema.TypeString,
 			Required:    true,
 			Description: "The URL to the API",
 			DefaultFunc: schema.EnvDefaultFunc("NS_URL", nil),
+		},
+		"insecure_skip_verify": {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Description: "Ignore validity of endpoint TLS certificate if true",
+			Default:     false,
 		},
 	}
 }
@@ -79,6 +85,10 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 		Endpoint: d.Get("endpoint").(string),
 	}
 	client := netscaler.NewNitroClient(c.Endpoint, c.Username, c.Password)
+	if d.Get("insecure_skip_verify").(bool) {
+		c.client.SkipCertificateVerification()
+	}
+
 	c.client = client
 
 	return &c, nil

.gitignore
@@ -2,3 +2,5 @@
 tags
 terraform-provider-netscaler
 tools/tools
+.idea
+*.iml

vendor/github.com/chiradeep/go-nitro/netscaler/client.go
@@ -21,6 +21,7 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"crypto/tls"
 )
 
 //NitroClient has methods to configure the NetScaler
@@ -31,6 +32,12 @@ type NitroClient struct {
 	password  string
 	proxiedNs string
 	client    *http.Client
+}
+
+func (client *NitroClient) SkipCertificateVerification() {
+	client.client.Transport = &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
 }
 
 //NewNitroClient returns a usable NitroClient. Does not check validity of supplied parameters

commit 28458389248e19bba57be8855114aa0bcdb87a34

fix compile issue introduced by d1b099719a6

netscaler/provider.go
@@ -58,12 +58,6 @@ func providerSchema() map[string]*schema.Schema {
 			Description: "The URL to the API",
 			DefaultFunc: schema.EnvDefaultFunc("NS_URL", nil),
 		},
-		"insecure_skip_verify": {
-			Type:        schema.TypeBool,
-			Optional:    true,
-			Description: "Ignore validity of endpoint TLS certificate if true",
-			Default:     false,
-		},
 	}
 }
 
@@ -87,9 +81,6 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 		Endpoint: d.Get("endpoint").(string),
 	}
 	client := netscaler.NewNitroClient(c.Endpoint, c.Username, c.Password)
-	if d.Get("insecure_skip_verify").(bool) {
-		client.SkipCertificateVerification()
-	}
 
 	c.client = client
 