47c5092b4485bdf7d16d3d2eb02c54604cad6065
Chiradeep Vittal <chiradeepv@gmail.com>
2018-05-15 22:25:57 -0700

    Support for inat rules

---
+++ netscaler/resource_inat_test.go
@@ -0,0 +1,121 @@
+/*
+Copyright 2016 Citrix Systems, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package netscaler
+
+import (
+	"fmt"
+	"github.com/chiradeep/go-nitro/netscaler"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"testing"
+)
+
+func TestAccInat_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckInatDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccInat_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckInatExist("netscaler_inat.foo", nil),
+
+					resource.TestCheckResourceAttr(
+						"netscaler_inat.foo", "name", "ip4ip"),
+					resource.TestCheckResourceAttr(
+						"netscaler_inat.foo", "privateip", "192.168.1.1"),
+					resource.TestCheckResourceAttr(
+						"netscaler_inat.foo", "publicip", "172.16.1.2"),
+					resource.TestCheckResourceAttr(
+						"netscaler_inat.foo", "tcpproxy", "ENABLED"),
+					resource.TestCheckResourceAttr(
+						"netscaler_inat.foo", "usnip", "ON"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckInatExist(n string, id *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No lb vserver name is set")
+		}
+
+		if id != nil {
+			if *id != "" && *id != rs.Primary.ID {
+				return fmt.Errorf("Resource ID has changed!")
+			}
+
+			*id = rs.Primary.ID
+		}
+
+		nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+		data, err := nsClient.FindResource(netscaler.Inat.Type(), rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		if data == nil {
+			return fmt.Errorf("Inat rule %s not found", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckInatDestroy(s *terraform.State) error {
+	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "netscaler_inat" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No name is set")
+		}
+
+		_, err := nsClient.FindResource(netscaler.Inat.Type(), rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("Inat rule %s still exists", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
+
+const testAccInat_basic = `
+
+
+resource "netscaler_inat" "foo" {
+  
+  name = "ip4ip"
+  privateip = "192.168.1.1"
+  publicip = "172.16.1.2"
+  tcpproxy = "ENABLED"
+  usnip = "ON"
+
+}
+`
