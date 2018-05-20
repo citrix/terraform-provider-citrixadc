554bdb4eef58e4d0cc26924de7140953af932fe6
Chiradeep Vittal <chiradeepv@gmail.com>
2018-05-19 21:23:12 -0700

    Add example for gslb

--- examples/gslb/provider.tf
+++ examples/gslb/provider.tf
@@ -1,3 +1,3 @@
 provider "netscaler" {
-    endpoint = "http://ec2-53-43-197-98.us-west-2.compute.amazonaws.com"
+    endpoint = "http://ec2-52-42-196-97.us-west-2.compute.amazonaws.com"
 }
--- examples/gslb/resources.tf
+++ examples/gslb/resources.tf
@@ -3,6 +3,28 @@ resource "netscaler_gslbsite" "site_local" {
   sitename = "Site-Local"
   siteipaddress = "172.31.96.234"
   sessionexchange = "DISABLED"
-  
+}
+
+resource "netscaler_gslbservice" "gslb_svc1" {
+  ip = "172.16.1.121"
+  port = "80"
+  servicename = "gslb1vservice"
+  servicetype = "HTTP"
+  sitename = "${netscaler_gslbsite.site_local.sitename}"
+}
+
+resource "netscaler_gslbvserver" "foo" {
+
+  dnsrecordtype = "A"
+  name = "GSLB-East-Coast-Vserver"
+  servicetype = "HTTP"
+  domain {
+	  domainname =  "www.fooco.co"
+	  ttl = "60"
+  }
+  domain {
+	  domainname = "www.barco.com"
+	  ttl = "55"
+  }
 }
 
