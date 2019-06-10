286139668fc80346699106cd378103681d2807e9
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2019-06-10 16:43:29 +0300

    Add helper test functions
    Signed-off-by: George Nikolopoulos <giorgos.nikolopoulos@citrix.com>

--- netscaler/resource_lbvserver_test.go
+++ netscaler/resource_lbvserver_test.go
@@ -16,16 +16,11 @@ limitations under the License.
 package netscaler
 
 import (
-	"encoding/base64"
 	"fmt"
-	"github.com/chiradeep/go-nitro/config/system"
 	"github.com/chiradeep/go-nitro/netscaler"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-	"io/ioutil"
 	"log"
-	"path"
-	"runtime"
 	"strings"
 	"testing"
 	"time"
@@ -169,37 +164,6 @@ func doPreChecks(t *testing.T) {
 		}
 	}
 
-}
-
-func uploadTestdataFile(t *testing.T, filename, targetDir string) error {
-	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
-
-	// Get here path
-	_, here_filename, _, _ := runtime.Caller(1)
-	b, err := ioutil.ReadFile(path.Join(path.Dir(here_filename), "testdata", filename))
-
-	if err != nil {
-		return err
-	}
-
-	sf := system.Systemfile{
-		Filename:     filename,
-		Filecontent:  base64.StdEncoding.EncodeToString(b),
-		Filelocation: targetDir,
-	}
-	_, err = nsClient.AddResource(netscaler.Systemfile.Type(), filename, &sf)
-	if err != nil && strings.Contains(err.Error(), "File already exists") {
-		args := map[string]string{"filelocation": "%2Fvar%2Ftmp"}
-		err := nsClient.DeleteResourceWithArgsMap(netscaler.Systemfile.Type(), filename, args)
-		if err != nil {
-			return err
-		}
-		_, err = nsClient.AddResource(netscaler.Systemfile.Type(), filename, &sf)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 const testAccLbvserver_basic = `
