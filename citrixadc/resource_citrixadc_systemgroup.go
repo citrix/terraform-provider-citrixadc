708751b4c32d69131369060473a62ec73068680e
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2020-02-12 16:58:20 +0200

    Add resource systemgroup
    Signed-off-by: George Nikolopoulos <giorgos.nikolopoulos@citrix.com>

---
+++ citrixadc/resource_citrixadc_systemgroup.go
@@ -0,0 +1,368 @@
+package citrixadc
+
+import (
+	"github.com/chiradeep/go-nitro/config/system"
+
+	"github.com/chiradeep/go-nitro/netscaler"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/hashcode"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"bytes"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func resourceCitrixAdcSystemgroup() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createSystemgroupFunc,
+		Read:          readSystemgroupFunc,
+		Update:        updateSystemgroupFunc,
+		Delete:        deleteSystemgroupFunc,
+		Schema: map[string]*schema.Schema{
+			"groupname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"promptstring": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"timeout": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"systemusers": &schema.Schema{
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"cmdpolicybinding": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Computed: false,
+				Set:      systemgroupCmdpolicybindingMappingHash,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"policyname": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
+							Computed: true,
+						},
+						"priority": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func createSystemgroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createSystemgroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	systemgroupName := d.Get("groupname").(string)
+
+	systemgroup := system.Systemgroup{
+		Groupname:    d.Get("groupname").(string),
+		Promptstring: d.Get("promptstring").(string),
+		Timeout:      d.Get("timeout").(int),
+	}
+
+	_, err := client.AddResource(netscaler.Systemgroup.Type(), systemgroupName, &systemgroup)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(systemgroupName)
+
+	err = updateSystemgroupCmdpolicyBindings(d, meta)
+	if err != nil {
+		return err
+	}
+
+	err = updateSystemgroupSystemuserBindings(d, meta)
+	if err != nil {
+		return err
+	}
+
+	err = readSystemgroupFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this systemgroup but we can't read it ?? %s", systemgroupName)
+		return nil
+	}
+	return nil
+}
+
+func readSystemgroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readSystemgroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	systemgroupName := d.Id()
+	log.Printf("[DEBUG] citrixadc-provider: Reading systemgroup state %s", systemgroupName)
+	data, err := client.FindResource(netscaler.Systemgroup.Type(), systemgroupName)
+	if err != nil {
+		log.Printf("[WARN] citrixadc-provider: Clearing systemgroup state %s", systemgroupName)
+		d.SetId("")
+		return nil
+	}
+
+	err = readSystemgroupCmdpolicybindings(d, meta)
+	if err != nil {
+		return err
+	}
+
+	err = readSystemgroupSystemuserbindings(d, meta)
+	if err != nil {
+		return err
+	}
+
+	d.Set("name", data["name"])
+	d.Set("groupname", data["groupname"])
+	d.Set("promptstring", data["promptstring"])
+	d.Set("timeout", data["timeout"])
+
+	return nil
+
+}
+
+func updateSystemgroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In updateSystemgroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	systemgroupName := d.Get("groupname").(string)
+
+	systemgroup := system.Systemgroup{
+		Groupname: d.Get("groupname").(string),
+	}
+	hasChange := false
+	if d.HasChange("promptstring") {
+		log.Printf("[DEBUG]  citrixadc-provider: Promptstring has changed for systemgroup %s, starting update", systemgroupName)
+		systemgroup.Promptstring = d.Get("promptstring").(string)
+		hasChange = true
+	}
+	if d.HasChange("timeout") {
+		log.Printf("[DEBUG]  citrixadc-provider: Timeout has changed for systemgroup %s, starting update", systemgroupName)
+		systemgroup.Timeout = d.Get("timeout").(int)
+		hasChange = true
+	}
+
+	if hasChange {
+		_, err := client.UpdateResource(netscaler.Systemgroup.Type(), systemgroupName, &systemgroup)
+		if err != nil {
+			return fmt.Errorf("Error updating systemgroup %s", systemgroupName)
+		}
+	}
+	if d.HasChange("cmdpolicybinding") {
+		err := updateSystemgroupCmdpolicyBindings(d, meta)
+		if err != nil {
+			return err
+		}
+	}
+
+	if d.HasChange("systemusers") {
+		err := updateSystemgroupSystemuserBindings(d, meta)
+		if err != nil {
+			return err
+		}
+	}
+	return readSystemgroupFunc(d, meta)
+}
+
+func deleteSystemgroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteSystemgroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	systemgroupName := d.Id()
+	err := client.DeleteResource(netscaler.Systemgroup.Type(), systemgroupName)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func deleteSingleSystemgroupCmdpolicyBinding(d *schema.ResourceData, meta interface{}, binding map[string]interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteSingleSystemgroupCmdpolicyBinding")
+	client := meta.(*NetScalerNitroClient).client
+
+	groupname := d.Get("groupname").(string)
+	// Construct args from binding data
+	args := make([]string, 0, 1)
+
+	if d, ok := binding["policyname"]; ok {
+		s := fmt.Sprintf("policyname:%v", d.(string))
+		args = append(args, s)
+	}
+
+	log.Printf("args is %v", args)
+
+	if err := client.DeleteResourceWithArgs("systemgroup_systemcmdpolicy_binding", groupname, args); err != nil {
+		log.Printf("[DEBUG]  citrixadc-provider: Error deleting cmdpolicy binding %v\n", binding)
+		return err
+	}
+
+	return nil
+}
+
+func addSingleSystemgroupCmdpolicyBinding(d *schema.ResourceData, meta interface{}, binding map[string]interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In addSingleSystemgroupCmdpolicyBinding")
+	client := meta.(*NetScalerNitroClient).client
+	log.Printf("Adding binding %v", binding)
+
+	bindingStruct := system.Systemgroupsystemcmdpolicybinding{}
+	bindingStruct.Groupname = d.Get("groupname").(string)
+
+	if d, ok := binding["policyname"]; ok {
+		bindingStruct.Policyname = d.(string)
+	}
+
+	if d, ok := binding["priority"]; ok {
+		bindingStruct.Priority = d.(int)
+	}
+
+	// We need to do a HTTP PUT hence the UpdateResource
+	if _, err := client.UpdateResource("systemgroup_systemcmdpolicy_binding", bindingStruct.Groupname, bindingStruct); err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateSystemgroupCmdpolicyBindings(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In updateSystemgroupCmdpolicyBindings")
+	oldSet, newSet := d.GetChange("cmdpolicybinding")
+	log.Printf("[DEBUG]  citrixadc-provider: oldSet %v\n", oldSet)
+	log.Printf("[DEBUG]  citrixadc-provider: newSet %v\n", newSet)
+	remove := oldSet.(*schema.Set).Difference(newSet.(*schema.Set))
+	add := newSet.(*schema.Set).Difference(oldSet.(*schema.Set))
+	for _, binding := range remove.List() {
+		if err := deleteSingleSystemgroupCmdpolicyBinding(d, meta, binding.(map[string]interface{})); err != nil {
+			return err
+		}
+	}
+
+	for _, binding := range add.List() {
+		if err := addSingleSystemgroupCmdpolicyBinding(d, meta, binding.(map[string]interface{})); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func systemgroupCmdpolicybindingMappingHash(v interface{}) int {
+	log.Printf("[DEBUG]  citrixadc-provider: In systemgroupCmdpolicybindingMappingHash")
+	var buf bytes.Buffer
+
+	m := v.(map[string]interface{})
+	if d, ok := m["policyname"]; ok {
+		buf.WriteString(fmt.Sprintf("%s-", d.(string)))
+	}
+	if d, ok := m["priority"]; ok {
+		buf.WriteString(fmt.Sprintf("%d-", d.(int)))
+	}
+	return hashcode.String(buf.String())
+}
+
+func readSystemgroupCmdpolicybindings(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In readSystemgroupCmdpolicybindings")
+	client := meta.(*NetScalerNitroClient).client
+	groupname := d.Get("groupname").(string)
+	bindings, _ := client.FindResourceArray("systemgroup_systemcmdpolicy_binding", groupname)
+	log.Printf("bindings %v\n", bindings)
+
+	processedBindings := make([]interface{}, len(bindings))
+	for i, _ := range bindings {
+		processedBindings[i] = make(map[string]interface{})
+		processedBindings[i].(map[string]interface{})["policyname"] = bindings[i]["policyname"].(string)
+		processedBindings[i].(map[string]interface{})["priority"], _ = strconv.Atoi(bindings[i]["priority"].(string))
+	}
+
+	updatedSet := schema.NewSet(systemgroupCmdpolicybindingMappingHash, processedBindings)
+	log.Printf("updatedSet %v\n", updatedSet)
+	if err := d.Set("cmdpolicybinding", updatedSet); err != nil {
+		return err
+	}
+	return nil
+}
+
+func deleteSingleSystemgroupSystemuserBinding(d *schema.ResourceData, meta interface{}, username string) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteSingleSystemgroupSystemuserBinding")
+	client := meta.(*NetScalerNitroClient).client
+
+	groupname := d.Get("groupname").(string)
+	args := make([]string, 0, 1)
+
+	s := fmt.Sprintf("username:%s", username)
+	args = append(args, s)
+
+	log.Printf("args is %v", args)
+
+	if err := client.DeleteResourceWithArgs("systemgroup_systemuser_binding", groupname, args); err != nil {
+		log.Printf("[DEBUG]  citrixadc-provider: Error deleting systemuser binding %v\n", username)
+		return err
+	}
+
+	return nil
+}
+
+func addSingleSystemgroupSystemuserBinding(d *schema.ResourceData, meta interface{}, username string) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In addSingleSystemgroupSystemuserBinding")
+	client := meta.(*NetScalerNitroClient).client
+
+	bindingStruct := system.Systemgroupsystemuserbinding{}
+	bindingStruct.Groupname = d.Get("groupname").(string)
+	bindingStruct.Username = username
+
+	// We need to do a HTTP PUT hence the UpdateResource
+	if _, err := client.UpdateResource("systemgroup_systemuser_binding", bindingStruct.Groupname, bindingStruct); err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateSystemgroupSystemuserBindings(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In updateSystemgroupCmdpolicyBindings")
+	oldSet, newSet := d.GetChange("systemusers")
+	log.Printf("[DEBUG]  citrixadc-provider: oldSet %v\n", oldSet)
+	log.Printf("[DEBUG]  citrixadc-provider: newSet %v\n", newSet)
+	remove := oldSet.(*schema.Set).Difference(newSet.(*schema.Set))
+	add := newSet.(*schema.Set).Difference(oldSet.(*schema.Set))
+	for _, username := range remove.List() {
+		if err := deleteSingleSystemgroupSystemuserBinding(d, meta, username.(string)); err != nil {
+			return err
+		}
+	}
+
+	for _, username := range add.List() {
+		if err := addSingleSystemgroupSystemuserBinding(d, meta, username.(string)); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func readSystemgroupSystemuserbindings(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In readSystemgroupSystemuserbindings")
+	client := meta.(*NetScalerNitroClient).client
+	groupname := d.Get("groupname").(string)
+	bindings, _ := client.FindResourceArray("systemgroup_systemuser_binding", groupname)
+	log.Printf("bindings %v\n", bindings)
+
+	processedBindings := make([]interface{}, len(bindings))
+	for i, val := range bindings {
+		processedBindings[i] = val["username"].(string)
+	}
+
+	updatedSet := processedBindings
+	log.Printf("updatedSet %v\n", updatedSet)
+	if err := d.Set("systemusers", updatedSet); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ citrixadc/resource_citrixadc_systemgroup_test.go
@@ -0,0 +1,216 @@
+/*
+Copyright 2016 Citrix Systems, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package citrixadc
+
+import (
+	"fmt"
+	"github.com/chiradeep/go-nitro/netscaler"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"testing"
+)
+
+func TestAccSystemgroup_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckSystemgroupDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccSystemgroup_basic_step1,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSystemgroupExist("citrixadc_systemgroup.tf_systemgroup", nil),
+				),
+			},
+			resource.TestStep{
+				Config: testAccSystemgroup_basic_step2,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSystemgroupExist("citrixadc_systemgroup.tf_systemgroup", nil),
+				),
+			},
+			resource.TestStep{
+				Config: testAccSystemgroup_basic_step3,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSystemgroupExist("citrixadc_systemgroup.tf_systemgroup", nil),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckSystemgroupExist(n string, id *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No lb vserver name is set")
+		}
+
+		if id != nil {
+			if *id != "" && *id != rs.Primary.ID {
+				return fmt.Errorf("Resource ID has changed!")
+			}
+
+			*id = rs.Primary.ID
+		}
+
+		nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+		data, err := nsClient.FindResource(netscaler.Systemgroup.Type(), rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		if data == nil {
+			return fmt.Errorf("LB vserver %s not found", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckSystemgroupDestroy(s *terraform.State) error {
+	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "citrixadc_systemgroup" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No name is set")
+		}
+
+		_, err := nsClient.FindResource(netscaler.Systemgroup.Type(), rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("LB vserver %s still exists", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
+
+const testAccSystemgroup_basic_step1 = `
+
+resource "citrixadc_systemuser" "tf_user1" {
+    username = "tf_user1"
+    password = "tf_password"
+    timeout = 900
+
+}
+
+resource "citrixadc_systemuser" "tf_user2" {
+    username = "tf_user2"
+    password = "tf_password"
+    timeout = 900
+
+}
+
+resource "citrixadc_systemgroup" "tf_systemgroup" {
+    groupname = "testgroupname"
+    timeout = 999
+    promptstring = "bye>"
+
+    cmdpolicybinding { 
+        policyname = "superuser"
+        priority = 100
+    }
+
+    cmdpolicybinding { 
+        policyname = "network"
+        priority = 200
+    }
+
+    systemusers = [
+        citrixadc_systemuser.tf_user1.username,
+		citrixadc_systemuser.tf_user2.username,
+    ]
+}
+
+
+`
+
+const testAccSystemgroup_basic_step2 = `
+
+resource "citrixadc_systemuser" "tf_user1" {
+    username = "tf_user1"
+    password = "tf_password"
+    timeout = 900
+
+}
+
+resource "citrixadc_systemuser" "tf_user2" {
+    username = "tf_user2"
+    password = "tf_password"
+    timeout = 900
+
+}
+
+resource "citrixadc_systemgroup" "tf_systemgroup" {
+    groupname = "testgroupname"
+    timeout = 999
+    promptstring = "bye>"
+
+    cmdpolicybinding { 
+        policyname = "superuser"
+        priority = 200
+    }
+
+    cmdpolicybinding { 
+        policyname = "network"
+        priority = 100
+    }
+
+    systemusers = [
+		citrixadc_systemuser.tf_user2.username
+    ]
+}
+
+
+`
+const testAccSystemgroup_basic_step3 = `
+
+resource "citrixadc_systemuser" "tf_user1" {
+    username = "tf_user1"
+    password = "tf_password"
+    timeout = 900
+
+}
+
+resource "citrixadc_systemuser" "tf_user2" {
+    username = "tf_user2"
+    password = "tf_password"
+    timeout = 900
+
+}
+
+resource "citrixadc_systemgroup" "tf_systemgroup" {
+    groupname = "testgroupname"
+    timeout = 1000
+    promptstring = "hello>"
+
+    systemusers = [
+		citrixadc_systemuser.tf_user1.username,
+		citrixadc_systemuser.tf_user2.username,
+    ]
+}
+
+
+`
