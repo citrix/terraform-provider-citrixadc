60b50a17aad8b66f5db0229c47c287f44f58f0d9
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2021-02-12 13:54:48 +0200

    Add resource_citrixadc_lbvserver_service_binding
    Signed-off-by: George Nikolopoulos <giorgos.nikolopoulos@citrix.com>

---
+++ citrixadc/resource_citrixadc_lbvserver_service_binding.go
@@ -0,0 +1,154 @@
+package citrixadc
+
+import (
+	"github.com/chiradeep/go-nitro/config/lb"
+
+	"github.com/chiradeep/go-nitro/netscaler"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+	"strings"
+)
+
+func resourceCitrixAdcLbvserver_service_binding() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createLbvserver_service_bindingFunc,
+		Read:          readLbvserver_service_bindingFunc,
+		Delete:        deleteLbvserver_service_bindingFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"servicename": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"weight": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func createLbvserver_service_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createLbvserver_service_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+	name := d.Get("name").(string)
+	servicename := d.Get("servicename").(string)
+
+	bindingId := fmt.Sprintf("%s,%s", name, servicename)
+
+	lbvserver_service_binding := lb.Lbvserverservicebinding{
+		Name:        d.Get("name").(string),
+		Servicename: d.Get("servicename").(string),
+		Weight:      d.Get("weight").(int),
+	}
+
+	_, err := client.AddResource(netscaler.Lbvserver_service_binding.Type(), name, &lbvserver_service_binding)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(bindingId)
+
+	err = readLbvserver_service_bindingFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this lbvserver_service_binding but we can't read it ?? %v", bindingId)
+		return nil
+	}
+	return nil
+}
+
+func readLbvserver_service_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readLbvserver_service_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+	name := idSlice[0]
+	servicename := idSlice[1]
+
+	log.Printf("[DEBUG] citrixadc-provider: Reading lbvserver_service_binding state %v", bindingId)
+
+	findParams := netscaler.FindParams{
+		ResourceType:             "lbvserver_service_binding",
+		ResourceName:             name,
+		ResourceMissingErrorCode: 258,
+	}
+	dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+	// Unexpected error
+	if err != nil {
+		log.Printf("[DEBUG] citrixadc-provider: Error during FindResourceArrayWithParams %s", err.Error())
+		return err
+	}
+
+	// Resource is missing
+	if len(dataArr) == 0 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams returned empty array")
+		log.Printf("[WARN] citrixadc-provider: Clearing lbvserver_service_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+
+	// Iterate through results to find the one with the right monitor name
+	foundIndex := -1
+	for i, v := range dataArr {
+		if v["servicename"].(string) == servicename {
+			foundIndex = i
+			break
+		}
+	}
+
+	// Resource is missing
+	if foundIndex == -1 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams monitor name not found in array")
+		log.Printf("[WARN] citrixadc-provider: Clearing lbvserver_service_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+	// Fallthrough
+
+	data := dataArr[foundIndex]
+
+	d.Set("name", data["name"])
+	d.Set("servicename", data["servicename"])
+	d.Set("weight", data["weight"])
+
+	return nil
+
+}
+
+func deleteLbvserver_service_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteLbvserver_service_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	name := idSlice[0]
+	servicename := idSlice[1]
+
+	args := make([]string, 0)
+	args = append(args, fmt.Sprintf("servicename:%s", servicename))
+
+	err := client.DeleteResourceWithArgs("lbvserver_service_binding", name, args)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
---
+++ citrixadc/resource_citrixadc_lbvserver_service_binding_test.go
@@ -0,0 +1,170 @@
+/*
+Copyright 2016 Citrix Systems, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package citrixadc
+
+import (
+	"fmt"
+	"github.com/chiradeep/go-nitro/netscaler"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"strings"
+	"testing"
+)
+
+const testAccLbvserver_service_binding_basic_step1 = `
+resource "citrixadc_lbvserver" "tf_lbvserver" {
+  ipv46       = "10.10.10.33"
+  name        = "tf_lbvserver"
+  port        = 80
+  servicetype = "HTTP"
+}
+
+resource "citrixadc_service" "tf_service" {
+    name = "tf_service"
+    ip = "192.168.43.33"
+    servicetype  = "HTTP"
+    port = 80
+}
+
+resource "citrixadc_lbvserver_service_binding" "tf_binding" {
+  name = citrixadc_lbvserver.tf_lbvserver.name
+  servicename = citrixadc_service.tf_service.name
+  weight = 10
+}
+`
+
+const testAccLbvserver_service_binding_basic_step2 = `
+resource "citrixadc_lbvserver" "tf_lbvserver" {
+  ipv46       = "10.10.10.33"
+  name        = "tf_lbvserver"
+  port        = 80
+  servicetype = "HTTP"
+}
+
+resource "citrixadc_service" "tf_service" {
+    name = "tf_service"
+    ip = "192.168.43.33"
+    servicetype  = "HTTP"
+    port = 80
+}
+
+resource "citrixadc_lbvserver_service_binding" "tf_binding" {
+  name = citrixadc_lbvserver.tf_lbvserver.name
+  servicename = citrixadc_service.tf_service.name
+  weight = 20
+}
+`
+
+func TestAccLbvserver_service_binding_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLbvserver_service_bindingDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccLbvserver_service_binding_basic_step1,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLbvserver_service_bindingExist("citrixadc_lbvserver_service_binding.tf_binding", nil),
+				),
+			},
+			resource.TestStep{
+				Config: testAccLbvserver_service_binding_basic_step2,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLbvserver_service_bindingExist("citrixadc_lbvserver_service_binding.tf_binding", nil),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckLbvserver_service_bindingExist(n string, id *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No lbvserver_service_binding id is set")
+		}
+
+		if id != nil {
+			if *id != "" && *id != rs.Primary.ID {
+				return fmt.Errorf("Resource ID has changed!")
+			}
+
+			*id = rs.Primary.ID
+		}
+
+		client := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+		bindingId := rs.Primary.ID
+
+		idSlice := strings.SplitN(bindingId, ",", 2)
+
+		name := idSlice[0]
+		servicename := idSlice[1]
+
+		findParams := netscaler.FindParams{
+			ResourceType:             "lbvserver_service_binding",
+			ResourceName:             name,
+			ResourceMissingErrorCode: 258,
+		}
+		dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+		// Unexpected error
+		if err != nil {
+			return err
+		}
+
+		// Iterate through results to find the one with the right monitor name
+		found := false
+		for _, v := range dataArr {
+			if v["servicename"].(string) == servicename {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return fmt.Errorf("lbvserver_service_binding %s not found", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckLbvserver_service_bindingDestroy(s *terraform.State) error {
+	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "citrixadc_lbvserver_service_binding" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No name is set")
+		}
+
+		_, err := nsClient.FindResource(netscaler.Lbvserver_service_binding.Type(), rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("lbvserver_service_binding %s still exists", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
