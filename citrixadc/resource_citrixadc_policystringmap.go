9bf3a130fb0fb665cee3bb03f1585654831cd726
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2021-02-15 14:38:53 +0200

    Add policy stringmap with pattern bindings
    Signed-off-by: George Nikolopoulos <giorgos.nikolopoulos@citrix.com>

---
+++ citrixadc/resource_citrixadc_policystringmap.go
@@ -0,0 +1,119 @@
+package citrixadc
+
+import (
+	"github.com/chiradeep/go-nitro/config/policy"
+
+	"github.com/chiradeep/go-nitro/netscaler"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+)
+
+func resourceCitrixAdcPolicystringmap() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createPolicystringmapFunc,
+		Read:          readPolicystringmapFunc,
+		Update:        updatePolicystringmapFunc,
+		Delete:        deletePolicystringmapFunc,
+		Schema: map[string]*schema.Schema{
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func createPolicystringmapFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createPolicystringmapFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+	policystringmapName := d.Get("name").(string)
+
+	policystringmap := policy.Policystringmap{
+		Comment: d.Get("comment").(string),
+		Name:    d.Get("name").(string),
+	}
+
+	_, err := client.AddResource(netscaler.Policystringmap.Type(), policystringmapName, &policystringmap)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(policystringmapName)
+
+	err = readPolicystringmapFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this policystringmap but we can't read it ?? %s", policystringmapName)
+		return nil
+	}
+	return nil
+}
+
+func readPolicystringmapFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readPolicystringmapFunc")
+	client := meta.(*NetScalerNitroClient).client
+	policystringmapName := d.Id()
+	log.Printf("[DEBUG] citrixadc-provider: Reading policystringmap state %s", policystringmapName)
+	data, err := client.FindResource(netscaler.Policystringmap.Type(), policystringmapName)
+	if err != nil {
+		log.Printf("[WARN] citrixadc-provider: Clearing policystringmap state %s", policystringmapName)
+		d.SetId("")
+		return nil
+	}
+	d.Set("comment", data["comment"])
+	d.Set("name", data["name"])
+
+	return nil
+
+}
+
+func updatePolicystringmapFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In updatePolicystringmapFunc")
+	client := meta.(*NetScalerNitroClient).client
+	policystringmapName := d.Get("name").(string)
+
+	policystringmap := policy.Policystringmap{
+		Name: d.Get("name").(string),
+	}
+	hasChange := false
+	if d.HasChange("comment") {
+		log.Printf("[DEBUG]  citrixadc-provider: Comment has changed for policystringmap %s, starting update", policystringmapName)
+		policystringmap.Comment = d.Get("comment").(string)
+		hasChange = true
+	}
+	if d.HasChange("name") {
+		log.Printf("[DEBUG]  citrixadc-provider: Name has changed for policystringmap %s, starting update", policystringmapName)
+		policystringmap.Name = d.Get("name").(string)
+		hasChange = true
+	}
+
+	if hasChange {
+		_, err := client.UpdateResource(netscaler.Policystringmap.Type(), policystringmapName, &policystringmap)
+		if err != nil {
+			return fmt.Errorf("Error updating policystringmap %s", policystringmapName)
+		}
+	}
+	return readPolicystringmapFunc(d, meta)
+}
+
+func deletePolicystringmapFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deletePolicystringmapFunc")
+	client := meta.(*NetScalerNitroClient).client
+	policystringmapName := d.Id()
+	err := client.DeleteResource(netscaler.Policystringmap.Type(), policystringmapName)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
---
+++ citrixadc/resource_citrixadc_policystringmap_pattern_binding.go
@@ -0,0 +1,149 @@
+package citrixadc
+
+import (
+	"github.com/chiradeep/go-nitro/config/policy"
+
+	"github.com/chiradeep/go-nitro/netscaler"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+	"strings"
+)
+
+func resourceCitrixAdcPolicystringmap_pattern_binding() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createPolicystringmap_pattern_bindingFunc,
+		Read:          readPolicystringmap_pattern_bindingFunc,
+		Delete:        deletePolicystringmap_pattern_bindingFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"key": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"value": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func createPolicystringmap_pattern_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createPolicystringmap_pattern_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	name := d.Get("name").(string)
+	key := d.Get("key").(string)
+	bindingId := fmt.Sprintf("%s,%s", name, key)
+	policystringmap_pattern_binding := policy.Policystringmappatternbinding{
+		Key:   d.Get("key").(string),
+		Name:  d.Get("name").(string),
+		Value: d.Get("value").(string),
+	}
+
+	_, err := client.AddResource(netscaler.Policystringmap_pattern_binding.Type(), name, &policystringmap_pattern_binding)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(bindingId)
+
+	err = readPolicystringmap_pattern_bindingFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this policystringmap_pattern_binding but we can't read it ?? %s", bindingId)
+		return nil
+	}
+	return nil
+}
+
+func readPolicystringmap_pattern_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readPolicystringmap_pattern_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+	name := idSlice[0]
+	key := idSlice[1]
+	log.Printf("[DEBUG] citrixadc-provider: Reading policystringmap_pattern_binding state %s", bindingId)
+
+	findParams := netscaler.FindParams{
+		ResourceType:             "policystringmap_pattern_binding",
+		ResourceName:             name,
+		ResourceMissingErrorCode: 258,
+	}
+	dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+	// Unexpected error
+	if err != nil {
+		log.Printf("[DEBUG] citrixadc-provider: Error during FindResourceArrayWithParams %s", err.Error())
+		return err
+	}
+
+	// Resource is missing
+	if len(dataArr) == 0 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams returned empty array")
+		log.Printf("[WARN] citrixadc-provider: Clearing policystringmap_pattern_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+
+	// Iterate through results to find the one with the right monitor name
+	foundIndex := -1
+	for i, v := range dataArr {
+		if v["key"].(string) == key {
+			foundIndex = i
+			break
+		}
+	}
+
+	// Resource is missing
+	if foundIndex == -1 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams monitor name not found in array")
+		log.Printf("[WARN] citrixadc-provider: Clearing policystringmap_pattern_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+	// Fallthrough
+
+	data := dataArr[foundIndex]
+
+	d.Set("name", data["name"])
+	d.Set("key", data["key"])
+	d.Set("value", data["value"])
+
+	return nil
+
+}
+
+func deletePolicystringmap_pattern_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deletePolicystringmap_pattern_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	name := idSlice[0]
+	key := idSlice[1]
+
+	args := make([]string, 0)
+	args = append(args, fmt.Sprintf("key:%s", key))
+
+	err := client.DeleteResourceWithArgs("policystringmap_pattern_binding", name, args)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
