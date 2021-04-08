11604af3015d883068609eb542ad03c4e988bbd4
Sumanth Lingappa <sumanth.lingappa@citrix.com>
2021-04-08 19:05:37 +0530

    removed unwanted update function
    Signed-off-by: Sumanth Lingappa <sumanth.lingappa@citrix.com>

--- citrixadc/resource_citrixadc_lbvserver_appfwpolicy_binding.go
+++ citrixadc/resource_citrixadc_lbvserver_appfwpolicy_binding.go
@@ -6,7 +6,6 @@ import (
 	"github.com/chiradeep/go-nitro/netscaler"
 	"github.com/hashicorp/terraform/helper/schema"
 
-	"fmt"
 	"log"
 )
 
@@ -15,7 +14,6 @@ func resourceCitrixAdcLbvserver_appfwpolicy_binding() *schema.Resource {
 		SchemaVersion: 1,
 		Create:        createLbvserver_appfwpolicy_bindingFunc,
 		Read:          readLbvserver_appfwpolicy_bindingFunc,
-		Update:        updateLbvserver_appfwpolicy_bindingFunc,
 		Delete:        deleteLbvserver_appfwpolicy_bindingFunc,
 		Schema: map[string]*schema.Schema{
 			"name": &schema.Schema{
@@ -115,65 +113,6 @@ func readLbvserver_appfwpolicy_bindingFunc(d *schema.ResourceData, meta interfac
 
 	return nil
 
-}
-
-func updateLbvserver_appfwpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
-	log.Printf("[DEBUG]  citrixadc-provider: In updateLbvserver_appfwpolicy_bindingFunc")
-	client := meta.(*NetScalerNitroClient).client
-	lbvserver_appfwpolicy_bindingName := d.Get("name").(string)
-
-	lbvserver_appfwpolicy_binding := lb.Lbvserverappfwpolicybinding{
-		Name: d.Get("name").(string),
-	}
-	hasChange := false
-	if d.HasChange("bindpoint") {
-		log.Printf("[DEBUG]  citrixadc-provider: Bindpoint has changed for lbvserver_appfwpolicy_binding %s, starting update", lbvserver_appfwpolicy_bindingName)
-		lbvserver_appfwpolicy_binding.Bindpoint = d.Get("bindpoint").(string)
-		hasChange = true
-	}
-	if d.HasChange("gotopriorityexpression") {
-		log.Printf("[DEBUG]  citrixadc-provider: Gotopriorityexpression has changed for lbvserver_appfwpolicy_binding %s, starting update", lbvserver_appfwpolicy_bindingName)
-		lbvserver_appfwpolicy_binding.Gotopriorityexpression = d.Get("gotopriorityexpression").(string)
-		hasChange = true
-	}
-	if d.HasChange("invoke") {
-		log.Printf("[DEBUG]  citrixadc-provider: Invoke has changed for lbvserver_appfwpolicy_binding %s, starting update", lbvserver_appfwpolicy_bindingName)
-		lbvserver_appfwpolicy_binding.Invoke = d.Get("invoke").(bool)
-		hasChange = true
-	}
-	if d.HasChange("labelname") {
-		log.Printf("[DEBUG]  citrixadc-provider: Labelname has changed for lbvserver_appfwpolicy_binding %s, starting update", lbvserver_appfwpolicy_bindingName)
-		lbvserver_appfwpolicy_binding.Labelname = d.Get("labelname").(string)
-		hasChange = true
-	}
-	if d.HasChange("labeltype") {
-		log.Printf("[DEBUG]  citrixadc-provider: Labeltype has changed for lbvserver_appfwpolicy_binding %s, starting update", lbvserver_appfwpolicy_bindingName)
-		lbvserver_appfwpolicy_binding.Labeltype = d.Get("labeltype").(string)
-		hasChange = true
-	}
-	if d.HasChange("name") {
-		log.Printf("[DEBUG]  citrixadc-provider: Name has changed for lbvserver_appfwpolicy_binding %s, starting update", lbvserver_appfwpolicy_bindingName)
-		lbvserver_appfwpolicy_binding.Name = d.Get("name").(string)
-		hasChange = true
-	}
-	if d.HasChange("policyname") {
-		log.Printf("[DEBUG]  citrixadc-provider: Policyname has changed for lbvserver_appfwpolicy_binding %s, starting update", lbvserver_appfwpolicy_bindingName)
-		lbvserver_appfwpolicy_binding.Policyname = d.Get("policyname").(string)
-		hasChange = true
-	}
-	if d.HasChange("priority") {
-		log.Printf("[DEBUG]  citrixadc-provider: Priority has changed for lbvserver_appfwpolicy_binding %s, starting update", lbvserver_appfwpolicy_bindingName)
-		lbvserver_appfwpolicy_binding.Priority = d.Get("priority").(int)
-		hasChange = true
-	}
-
-	if hasChange {
-		_, err := client.UpdateResource(netscaler.Lbvserver_appfwpolicy_binding.Type(), lbvserver_appfwpolicy_bindingName, &lbvserver_appfwpolicy_binding)
-		if err != nil {
-			return fmt.Errorf("Error updating lbvserver_appfwpolicy_binding %s", lbvserver_appfwpolicy_bindingName)
-		}
-	}
-	return readLbvserver_appfwpolicy_bindingFunc(d, meta)
 }
 
 func deleteLbvserver_appfwpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
