f3f0b1a30274e6f7a40d6d7a4faf8ae6b823844b
Theodoros Tsakalos <theodoros.tsakalos@citrix.com>
2021-09-06 15:26:47 +0000

    Added sslservicegroup_sslcertkey_binding resource

---
+++ citrixadc/resource_citrixadc_sslservicegroup_sslcertkey_binding.go
@@ -0,0 +1,176 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/ssl"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+	"net/url"
+	"strings"
+)
+
+func resourceCitrixAdcSslservicegroup_sslcertkey_binding() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createSslservicegroup_sslcertkey_bindingFunc,
+		Read:          readSslservicegroup_sslcertkey_bindingFunc,
+		Delete:        deleteSslservicegroup_sslcertkey_bindingFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"ca": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"certkeyname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"crlcheck": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"ocspcheck": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"servicegroupname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"snicert": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func createSslservicegroup_sslcertkey_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createSslservicegroup_sslcertkey_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	servicegroupname := d.Get("servicegroupname").(string)
+	certkeyname := d.Get("certkeyname").(string)
+	bindingId := fmt.Sprintf("%s,%s", servicegroupname, certkeyname)
+	sslservicegroup_sslcertkey_binding := ssl.Sslservicegroupsslcertkeybinding{
+		Ca:               d.Get("ca").(bool),
+		Certkeyname:      d.Get("certkeyname").(string),
+		Crlcheck:         d.Get("crlcheck").(string),
+		Ocspcheck:        d.Get("ocspcheck").(string),
+		Servicegroupname: d.Get("servicegroupname").(string),
+		Snicert:          d.Get("snicert").(bool),
+	}
+
+	_, err := client.AddResource(service.Sslservicegroup_sslcertkey_binding.Type(), servicegroupname, &sslservicegroup_sslcertkey_binding)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(bindingId)
+
+	err = readSslservicegroup_sslcertkey_bindingFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this sslservicegroup_sslcertkey_binding but we can't read it ?? %s", bindingId)
+		return nil
+	}
+	return nil
+}
+
+func readSslservicegroup_sslcertkey_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readSslservicegroup_sslcertkey_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	servicegroupname := idSlice[0]
+	certkeyname := idSlice[1]
+
+	log.Printf("[DEBUG] citrixadc-provider: Reading sslservicegroup_sslcertkey_binding state %s", bindingId)
+
+	findParams := service.FindParams{
+		ResourceType:             "sslservicegroup_sslcertkey_binding",
+		ResourceName:             servicegroupname,
+		ResourceMissingErrorCode: 258,
+	}
+	dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+	// Unexpected error
+	if err != nil {
+		log.Printf("[DEBUG] citrixadc-provider: Error during FindResourceArrayWithParams %s", err.Error())
+		return err
+	}
+
+	// Resource is missing
+	if len(dataArr) == 0 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams returned empty array")
+		log.Printf("[WARN] citrixadc-provider: Clearing sslservicegroup_sslcertkey_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+
+	// Iterate through results to find the one with the right id
+	foundIndex := -1
+	for i, v := range dataArr {
+		if v["certkeyname"].(string) == certkeyname {
+			foundIndex = i
+			break
+		}
+	}
+
+	// Resource is missing
+	if foundIndex == -1 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams sslcertkey not found in array")
+		log.Printf("[WARN] citrixadc-provider: Clearing sslservicegroup_sslcertkey_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+	// Fallthrough
+
+	data := dataArr[foundIndex]
+
+	d.Set("ca", data["ca"])
+	d.Set("certkeyname", data["certkeyname"])
+	d.Set("crlcheck", data["crlcheck"])
+	d.Set("ocspcheck", data["ocspcheck"])
+	d.Set("servicegroupname", data["servicegroupname"])
+	d.Set("snicert", data["snicert"])
+
+	return nil
+
+}
+
+func deleteSslservicegroup_sslcertkey_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteSslservicegroup_sslcertkey_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	name := idSlice[0]
+	certkeyname := idSlice[1]
+
+	argsMap := make(map[string]string)
+	argsMap["certkeyname"] = url.QueryEscape(certkeyname)
+
+	err := client.DeleteResourceWithArgsMap(service.Sslservicegroup_sslcertkey_binding.Type(), name, argsMap)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
---
+++ citrixadc/resource_citrixadc_sslservicegroup_sslcertkey_binding_test.go
@@ -0,0 +1,199 @@
+/*
+Copyright 2016 Citrix Systems, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package citrixadc
+
+import (
+	"fmt"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"strings"
+	"testing"
+)
+
+const testAccSslservicegroup_sslcertkey_binding_basic = `
+	resource "citrixadc_sslservicegroup_sslcertkey_binding" "tf_sslservicegroup_sslcertkey_binding" {
+		ca = false
+        certkeyname = citrixadc_sslcertkey.tf_sslcertkey.certkey
+        servicegroupname = citrixadc_servicegroup.tf_servicegroup.servicegroupname
+	}
+
+	resource "citrixadc_sslcertkey" "tf_sslcertkey" {
+		certkey = "tf_sslcertkey"
+		cert = "/var/tmp/certificate1.crt"
+		key = "/var/tmp/key1.pem"
+	}
+
+	resource "citrixadc_servicegroup" "tf_servicegroup" {
+		servicegroupname = "tf_servicegroup"
+		servicetype = "SSL"
+	}
+`
+
+const testAccSslservicegroup_sslcertkey_binding_basic_step2 = `
+	resource "citrixadc_sslcertkey" "tf_sslcertkey" {
+		certkey = "tf_sslcertkey"
+		cert = "/var/tmp/certificate1.crt"
+		key = "/var/tmp/key1.pem"
+	}
+
+	resource "citrixadc_servicegroup" "tf_servicegroup" {
+		servicegroupname = "tf_servicegroup"
+		servicetype = "SSL"
+	}
+`
+
+func TestAccSslservicegroup_sslcertkey_binding_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { doSslcertkeyPreChecks(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckSslservicegroup_sslcertkey_bindingDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccSslservicegroup_sslcertkey_binding_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSslservicegroup_sslcertkey_bindingExist("citrixadc_sslservicegroup_sslcertkey_binding.tf_sslservicegroup_sslcertkey_binding", nil),
+				),
+			},
+			resource.TestStep{
+				Config: testAccSslservicegroup_sslcertkey_binding_basic_step2,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSslservicegroup_sslcertkey_bindingNotExist("citrixadc_sslservicegroup_sslcertkey_binding.tf_sslservicegroup_sslcertkey_binding", "tf_servicegroup,tf_sslcertkey"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckSslservicegroup_sslcertkey_bindingExist(n string, id *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No sslservicegroup_sslcertkey_binding id is set")
+		}
+
+		if id != nil {
+			if *id != "" && *id != rs.Primary.ID {
+				return fmt.Errorf("Resource ID has changed!")
+			}
+
+			*id = rs.Primary.ID
+		}
+
+		client := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+		bindingId := rs.Primary.ID
+
+		idSlice := strings.SplitN(bindingId, ",", 2)
+
+		servicegroupname := idSlice[0]
+		certkeyname := idSlice[1]
+
+		findParams := service.FindParams{
+			ResourceType:             "sslservicegroup_sslcertkey_binding",
+			ResourceName:             servicegroupname,
+			ResourceMissingErrorCode: 258,
+		}
+		dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+		// Unexpected error
+		if err != nil {
+			return err
+		}
+
+		// Iterate through results to find the one with the right monitor name
+		found := false
+		for _, v := range dataArr {
+			if v["certkeyname"].(string) == certkeyname {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return fmt.Errorf("sslservicegroup_sslcertkey_binding %s not found", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckSslservicegroup_sslcertkey_bindingNotExist(n string, id string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+		if !strings.Contains(id, ",") {
+			return fmt.Errorf("Invalid id string %v. The id string must contain a comma.", id)
+		}
+		idSlice := strings.SplitN(id, ",", 2)
+
+		name := idSlice[0]
+		certkeyname := idSlice[1]
+
+		findParams := service.FindParams{
+			ResourceType:             "sslservicegroup_sslcertkey_binding",
+			ResourceName:             name,
+			ResourceMissingErrorCode: 258,
+		}
+		dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+		// Unexpected error
+		if err != nil {
+			return err
+		}
+
+		// Iterate through results to find the one with the right certkey name
+		found := false
+		for _, v := range dataArr {
+			if v["certkeyname"].(string) == certkeyname {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			return fmt.Errorf("sslservicegroup_sslcertkey_binding %s was found, but is should have been destroyed", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckSslservicegroup_sslcertkey_bindingDestroy(s *terraform.State) error {
+	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "citrixadc_sslservicegroup_sslcertkey_binding" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No name is set")
+		}
+
+		_, err := nsClient.FindResource(service.Sslservicegroup_sslcertkey_binding.Type(), rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("sslservicegroup_sslcertkey_binding %s still exists", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
