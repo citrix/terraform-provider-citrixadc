8bf0b710d1ba4acccc9092533ef7a6f8563e1cd2
foteinii <foteini.iliopoulou@citrix.com>
2021-09-10 07:29:06 +0000

    Sslservice resource,example,doc files

---
+++ citrixadc/resource_citrixadc_sslservice_test.go
@@ -0,0 +1,237 @@
+/*
+Copyright 2016 Citrix Systems, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package citrixadc
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+const testAccSslservice_basic = `
+resource "citrixadc_sslservice" "demo_sslservice" {
+	cipherredirect = "DISABLED"
+	clientauth = "DISABLED"
+	dh = "DISABLED"
+	dhcount = 0
+	dhkeyexpsizelimit = "DISABLED"
+	ersa = "DISABLED"
+	redirectportrewrite = "DISABLED"
+	serverauth = "ENABLED"
+	servicename = citrixadc_service.tf_service.name
+	sessreuse = "ENABLED"
+	snienable = "DISABLED"
+	ssl2 = "DISABLED"
+	ssl3 = "ENABLED"
+	sslredirect = "DISABLED"
+	sslv2redirect = "DISABLED"
+	tls1 = "DISABLED"
+	tls11 = "ENABLED"
+	tls12 = "ENABLED"
+	tls13 = "DISABLED"
+	sesstimeout = 380
+
+}
+
+resource "citrixadc_lbvserver" "tf_lbvserver" {
+	ipv46       = "10.10.10.44"
+	name        = "tf_lbvserver"
+	port        = 443
+	servicetype = "SSL"
+	sslprofile  = "ns_default_ssl_profile_frontend"
+}
+
+resource "citrixadc_service" "tf_service" {
+	name = "tf_service"
+	servicetype = "SSL"
+	port = 443 
+	lbvserver = citrixadc_lbvserver.tf_lbvserver.name
+	ip = "10.77.33.22"
+}
+`
+
+const testAccSslservice_basic_update_sess = `
+resource "citrixadc_sslservice" "demo_sslservice" {
+	cipherredirect = "DISABLED"
+	clientauth = "DISABLED"
+	dh = "DISABLED"
+	dhcount = 0
+	dhkeyexpsizelimit = "DISABLED"
+	ersa = "DISABLED"
+	redirectportrewrite = "DISABLED"
+	serverauth = "ENABLED"
+	servicename = citrixadc_service.tf_service.name
+	sessreuse = "ENABLED"
+	snienable = "DISABLED"
+	ssl2 = "DISABLED"
+	ssl3 = "ENABLED"
+	sslredirect = "DISABLED"
+	sslv2redirect = "DISABLED"
+	tls1 = "DISABLED"
+	tls11 = "ENABLED"
+	tls12 = "ENABLED"
+	tls13 = "DISABLED"
+	sesstimeout = 390
+
+}
+
+resource "citrixadc_lbvserver" "tf_lbvserver" {
+	ipv46       = "10.10.10.44"
+	name        = "tf_lbvserver"
+	port        = 443
+	servicetype = "SSL"
+	sslprofile  = "ns_default_ssl_profile_frontend"
+}
+
+resource "citrixadc_service" "tf_service" {
+	name = "tf_service"
+	servicetype = "SSL"
+	port = 443 
+	lbvserver = citrixadc_lbvserver.tf_lbvserver.name
+	ip = "10.77.33.22"
+}
+`
+const testAccSslservice_basic_update_general = `
+resource "citrixadc_sslservice" "demo_sslservice" {
+	cipherredirect = "DISABLED"
+	clientauth = "DISABLED"
+	dh = "DISABLED"
+	dhcount = 0
+	dhkeyexpsizelimit = "DISABLED"
+	ersa = "DISABLED"
+	redirectportrewrite = "DISABLED"
+	serverauth = "ENABLED"
+	servicename = citrixadc_service.tf_service.name
+	sessreuse = "DISABLED"
+	snienable = "DISABLED"
+	ssl2 = "DISABLED"
+	ssl3 = "ENABLED"
+	sslredirect = "DISABLED"
+	sslv2redirect = "DISABLED"
+	tls1 = "DISABLED"
+	tls11 = "ENABLED"
+	tls12 = "ENABLED"
+	tls13 = "DISABLED"
+	sesstimeout = 390
+
+}
+
+resource "citrixadc_lbvserver" "tf_lbvserver" {
+	ipv46       = "10.10.10.44"
+	name        = "tf_lbvserver"
+	port        = 443
+	servicetype = "SSL"
+	sslprofile  = "ns_default_ssl_profile_frontend"
+}
+
+resource "citrixadc_service" "tf_service" {
+	name = "tf_service"
+	servicetype = "SSL"
+	port = 443 
+	lbvserver = citrixadc_lbvserver.tf_lbvserver.name
+	ip = "10.77.33.22"
+}
+`
+
+func TestAccSslservice_basic(t *testing.T) {
+	if adcTestbed != "STANDALONE_NON_DEFAULT_SSL_PROFILE" {
+		t.Skipf("ADC testbed is %s. Expected STANDALONE_NON_DEFAULT_SSL_PROFILE.", adcTestbed)
+	}
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckSslserviceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccSslservice_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSslserviceExist("citrixadc_sslservice.demo_sslservice", nil),
+				),
+			},
+			resource.TestStep{
+				Config: testAccSslservice_basic_update_sess,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSslserviceExist("citrixadc_sslservice.demo_sslservice", nil),
+				),
+			},
+			resource.TestStep{
+				Config: testAccSslservice_basic_update_general,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSslserviceExist("citrixadc_sslservice.demo_sslservice", nil),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckSslserviceExist(n string, id *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No sslservice name is set")
+		}
+
+		if id != nil {
+			if *id != "" && *id != rs.Primary.ID {
+				return fmt.Errorf("Resource ID has changed!")
+			}
+
+			*id = rs.Primary.ID
+		}
+
+		nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+		data, err := nsClient.FindResource(service.Sslservice.Type(), rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		if data == nil {
+			return fmt.Errorf("sslservice %s not found", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckSslserviceDestroy(s *terraform.State) error {
+	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "citrixadc_sslservice" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No name is set")
+		}
+
+		_, err := nsClient.FindResource(service.Sslservice.Type(), rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("sslservice %s still exists", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
