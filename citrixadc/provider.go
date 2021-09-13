02fd98f58496aae3713be9fd0923ff5071127e8a
foteinii <foteini.iliopoulou@citrix.com>
2021-09-13 14:05:20 +0000

    sslprofile_sslcertkey_binding resource,example,doc files

--- citrixadc/provider.go
+++ citrixadc/provider.go
@@ -83,6 +83,7 @@ func providerResources() map[string]*schema.Resource {
 		"citrixadc_lbroute6":                                           resourceCitrixAdcLbroute6(),
 		"citrixadc_sslpolicylabel":                                     resourceCitrixAdcSslpolicylabel(),
 		"citrixadc_ssllogprofile":                                      resourceCitrixAdcSsllogprofile(),
+		"citrixadc_sslprofile_sslcertkey_binding":                      resourceCitrixAdcSslprofile_sslcertkey_binding(),
 		"citrixadc_lbvserver_botpolicy_binding":                        resourceCitrixAdcLbvserver_botpolicy_binding(),
 		"citrixadc_lbvserver_auditsyslogpolicy_binding":                resourceCitrixAdcLbvserver_auditsyslogpolicy_binding(),
 		"citrixadc_sslcacertgroup":                                     resourceCitrixAdcSslcacertgroup(),
