397b883da0a5f2c1139a60ee8128e4dd56057651
George Nikolopoulos <giorgos.nikolopoulos@citrix.com>
2022-01-11 13:17:04 +0200

    Add authorization go-nitro definitions

---
+++ vendor/github.com/citrix/adc-nitro-go/resource/config/authorization/authorizationpolicylabel.go
@@ -0,0 +1,47 @@
+/*
+* Copyright (c) 2021 Citrix Systems, Inc.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+
+package authorization
+
+/**
+* Configuration for authorization policy label resource.
+*/
+type Authorizationpolicylabel struct {
+	/**
+	* Name for the new authorization policy label. 
+		Must begin with a letter, number, or the underscore character (_), and must contain only letters, numbers, and the hyphen (-), period (.) pound (#), space ( ), at (@), equals (=), colon (:), and underscore characters. Cannot be changed after the authorization policy is created.
+		The following requirement applies only to the Citrix ADC CLI:
+		If the name includes one or more spaces, enclose the name in double or single quotation marks (for example, "my authorization policy label" or 'authorization policy label').
+	*/
+	Labelname string `json:"labelname,omitempty"`
+	/**
+	* The new name of the auth policy label
+	*/
+	Newname string `json:"newname,omitempty"`
+
+	//------- Read only Parameter ---------;
+
+	Numpol string `json:"numpol,omitempty"`
+	Hits string `json:"hits,omitempty"`
+	Policyname string `json:"policyname,omitempty"`
+	Priority string `json:"priority,omitempty"`
+	Gotopriorityexpression string `json:"gotopriorityexpression,omitempty"`
+	Labeltype string `json:"labeltype,omitempty"`
+	Invokelabelname string `json:"invoke_labelname,omitempty"`
+	Flowtype string `json:"flowtype,omitempty"`
+	Description string `json:"description,omitempty"`
+
+}
---
+++ vendor/github.com/citrix/adc-nitro-go/resource/config/authorization/authorizationpolicylabel_policy_binding.go
@@ -0,0 +1,56 @@
+/*
+* Copyright (c) 2021 Citrix Systems, Inc.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+
+package authorization
+
+/**
+* Binding class showing the policy that can be bound to authorizationpolicylabel.
+*/
+type Authorizationpolicylabelpolicybinding struct {
+	/**
+	* Name of the authorization policy to bind to the policy label.
+	*/
+	Policyname string `json:"policyname,omitempty"`
+	/**
+	* Specifies the priority of the policy.
+	*/
+	Priority uint32 `json:"priority,omitempty"`
+	/**
+	* Expression specifying the priority of the next policy which will get evaluated if the current policy rule evaluates to TRUE.
+	*/
+	Gotopriorityexpression string `json:"gotopriorityexpression,omitempty"`
+	/**
+	* Type of invocation. Available settings function as follows:
+		* reqvserver - Send the request to the specified request virtual server.
+		* resvserver - Send the response to the specified response virtual server.
+		* policylabel - Invoke the specified policy label.
+	*/
+	Labeltype string `json:"labeltype,omitempty"`
+	/**
+	* Name of the policy label to invoke if the current policy evaluates to TRUE, the invoke parameter is set, and Label Type is set to Policy Label.
+	*/
+	Invokelabelname string `json:"invoke_labelname,omitempty"`
+	/**
+	* Name of the authorization policy label to which to bind the policy.
+	*/
+	Labelname string `json:"labelname,omitempty"`
+	/**
+	* If the current policy evaluates to TRUE, terminate evaluation of policies bound to the current policy label, and then either forward the request or response to the specified virtual server or evaluate the specified policy label.
+	*/
+	Invoke bool `json:"invoke,omitempty"`
+
+
+}
