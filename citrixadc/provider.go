30d4b4f2c813291003f07ad6638343d15c1c0ca0
Rohit Myali <rohit.myali@citrix.com>
2022-02-24 08:53:06 +0000

    Added vpnvserver_auditnslogpolicy_binding Resource

--- citrixadc/provider.go
+++ citrixadc/provider.go
@@ -321,6 +321,7 @@ func providerResources() map[string]*schema.Resource {
 		"citrixadc_vpnglobal_auditsyslogpolicy_binding":                    resourceCitrixAdcVpnglobal_auditsyslogpolicy_binding(),
 		"citrixadc_vpnglobal_vpnintranetapplication_binding":               resourceCitrixAdcVpnglobal_vpnintranetapplication_binding(),
 		"citrixadc_vpnvserver_auditsyslogpolicy_binding":                   resourceCitrixAdcVpnvserver_auditsyslogpolicy_binding(),
+		"citrixadc_vpnvserver_auditnslogpolicy_binding":                    resourceCitrixAdcVpnvserver_auditnslogpolicy_binding(),
 	}
 }
 
