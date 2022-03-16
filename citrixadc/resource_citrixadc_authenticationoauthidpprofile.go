3d156ef19f826077464e472daf0445057fc9f293
Rohit Myali <rohit.myali@citrix.com>
2022-03-16 06:19:36 +0000

    Added authenticationoauthidpprofile Resource

---
+++ citrixadc/resource_citrixadc_authenticationoauthidpprofile.go
@@ -0,0 +1,283 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/authentication"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+)
+
+func resourceCitrixAdcAuthenticationoauthidpprofile() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createAuthenticationoauthidpprofileFunc,
+		Read:          readAuthenticationoauthidpprofileFunc,
+		Update:        updateAuthenticationoauthidpprofileFunc,
+		Delete:        deleteAuthenticationoauthidpprofileFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				Computed: false,
+				ForceNew: true,
+			},
+			"clientid": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				Computed: false,
+			},
+			"clientsecret": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				Computed: false,
+			},
+			"redirecturl": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				Computed: false,
+			},
+			"attributes": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"audience": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"configservice": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"defaultauthenticationgroup": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"encrypttoken": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"issuer": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"refreshinterval": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"relyingpartymetadataurl": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"sendpassword": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"signaturealg": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"signatureservice": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"skewtime": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func createAuthenticationoauthidpprofileFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createAuthenticationoauthidpprofileFunc")
+	client := meta.(*NetScalerNitroClient).client
+	authenticationoauthidpprofileName := d.Get("name").(string)
+	authenticationoauthidpprofile := authentication.Authenticationoauthidpprofile{
+		Attributes:                 d.Get("attributes").(string),
+		Audience:                   d.Get("audience").(string),
+		Clientid:                   d.Get("clientid").(string),
+		Clientsecret:               d.Get("clientsecret").(string),
+		Configservice:              d.Get("configservice").(string),
+		Defaultauthenticationgroup: d.Get("defaultauthenticationgroup").(string),
+		Encrypttoken:               d.Get("encrypttoken").(string),
+		Issuer:                     d.Get("issuer").(string),
+		Name:                       d.Get("name").(string),
+		Redirecturl:                d.Get("redirecturl").(string),
+		Refreshinterval:            d.Get("refreshinterval").(int),
+		Relyingpartymetadataurl:    d.Get("relyingpartymetadataurl").(string),
+		Sendpassword:               d.Get("sendpassword").(string),
+		Signaturealg:               d.Get("signaturealg").(string),
+		Signatureservice:           d.Get("signatureservice").(string),
+		Skewtime:                   d.Get("skewtime").(int),
+	}
+
+	_, err := client.AddResource("authenticationoauthidpprofile", authenticationoauthidpprofileName, &authenticationoauthidpprofile)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(authenticationoauthidpprofileName)
+
+	err = readAuthenticationoauthidpprofileFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this authenticationoauthidpprofile but we can't read it ?? %s", authenticationoauthidpprofileName)
+		return nil
+	}
+	return nil
+}
+
+func readAuthenticationoauthidpprofileFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readAuthenticationoauthidpprofileFunc")
+	client := meta.(*NetScalerNitroClient).client
+	authenticationoauthidpprofileName := d.Id()
+	log.Printf("[DEBUG] citrixadc-provider: Reading authenticationoauthidpprofile state %s", authenticationoauthidpprofileName)
+	data, err := client.FindResource("authenticationoauthidpprofile", authenticationoauthidpprofileName)
+	if err != nil {
+		log.Printf("[WARN] citrixadc-provider: Clearing authenticationoauthidpprofile state %s", authenticationoauthidpprofileName)
+		d.SetId("")
+		return nil
+	}
+	d.Set("attributes", data["attributes"])
+	d.Set("audience", data["audience"])
+	d.Set("clientid", data["clientid"])
+	// d.Set("clientsecret", data["clientsecret"]) Every time it changes when recieved from ADC
+	d.Set("configservice", data["configservice"])
+	d.Set("defaultauthenticationgroup", data["defaultauthenticationgroup"])
+	d.Set("encrypttoken", data["encrypttoken"])
+	d.Set("issuer", data["issuer"])
+	d.Set("name", data["name"])
+	d.Set("redirecturl", data["redirecturl"])
+	d.Set("refreshinterval", data["refreshinterval"])
+	d.Set("relyingpartymetadataurl", data["relyingpartymetadataurl"])
+	d.Set("sendpassword", data["sendpassword"])
+	d.Set("signaturealg", data["signaturealg"])
+	d.Set("signatureservice", data["signatureservice"])
+	d.Set("skewtime", data["skewtime"])
+
+	return nil
+
+}
+
+func updateAuthenticationoauthidpprofileFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In updateAuthenticationoauthidpprofileFunc")
+	client := meta.(*NetScalerNitroClient).client
+	authenticationoauthidpprofileName := d.Get("name").(string)
+
+	authenticationoauthidpprofile := authentication.Authenticationoauthidpprofile{
+		Name: d.Get("name").(string),
+	}
+	hasChange := false
+	if d.HasChange("attributes") {
+		log.Printf("[DEBUG]  citrixadc-provider: Attributes has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Attributes = d.Get("attributes").(string)
+		hasChange = true
+	}
+	if d.HasChange("audience") {
+		log.Printf("[DEBUG]  citrixadc-provider: Audience has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Audience = d.Get("audience").(string)
+		hasChange = true
+	}
+	if d.HasChange("clientid") {
+		log.Printf("[DEBUG]  citrixadc-provider: Clientid has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Clientid = d.Get("clientid").(string)
+		hasChange = true
+	}
+	if d.HasChange("clientsecret") {
+		log.Printf("[DEBUG]  citrixadc-provider: Clientsecret has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Clientsecret = d.Get("clientsecret").(string)
+		hasChange = true
+	}
+	if d.HasChange("configservice") {
+		log.Printf("[DEBUG]  citrixadc-provider: Configservice has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Configservice = d.Get("configservice").(string)
+		hasChange = true
+	}
+	if d.HasChange("defaultauthenticationgroup") {
+		log.Printf("[DEBUG]  citrixadc-provider: Defaultauthenticationgroup has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Defaultauthenticationgroup = d.Get("defaultauthenticationgroup").(string)
+		hasChange = true
+	}
+	if d.HasChange("encrypttoken") {
+		log.Printf("[DEBUG]  citrixadc-provider: Encrypttoken has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Encrypttoken = d.Get("encrypttoken").(string)
+		hasChange = true
+	}
+	if d.HasChange("issuer") {
+		log.Printf("[DEBUG]  citrixadc-provider: Issuer has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Issuer = d.Get("issuer").(string)
+		hasChange = true
+	}
+	if d.HasChange("redirecturl") {
+		log.Printf("[DEBUG]  citrixadc-provider: Redirecturl has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Redirecturl = d.Get("redirecturl").(string)
+		hasChange = true
+	}
+	if d.HasChange("refreshinterval") {
+		log.Printf("[DEBUG]  citrixadc-provider: Refreshinterval has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Refreshinterval = d.Get("refreshinterval").(int)
+		hasChange = true
+	}
+	if d.HasChange("relyingpartymetadataurl") {
+		log.Printf("[DEBUG]  citrixadc-provider: Relyingpartymetadataurl has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Relyingpartymetadataurl = d.Get("relyingpartymetadataurl").(string)
+		hasChange = true
+	}
+	if d.HasChange("sendpassword") {
+		log.Printf("[DEBUG]  citrixadc-provider: Sendpassword has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Sendpassword = d.Get("sendpassword").(string)
+		hasChange = true
+	}
+	if d.HasChange("signaturealg") {
+		log.Printf("[DEBUG]  citrixadc-provider: Signaturealg has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Signaturealg = d.Get("signaturealg").(string)
+		hasChange = true
+	}
+	if d.HasChange("signatureservice") {
+		log.Printf("[DEBUG]  citrixadc-provider: Signatureservice has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Signatureservice = d.Get("signatureservice").(string)
+		hasChange = true
+	}
+	if d.HasChange("skewtime") {
+		log.Printf("[DEBUG]  citrixadc-provider: Skewtime has changed for authenticationoauthidpprofile %s, starting update", authenticationoauthidpprofileName)
+		authenticationoauthidpprofile.Skewtime = d.Get("skewtime").(int)
+		hasChange = true
+	}
+
+	if hasChange {
+		_, err := client.UpdateResource("authenticationoauthidpprofile", authenticationoauthidpprofileName, &authenticationoauthidpprofile)
+		if err != nil {
+			return fmt.Errorf("Error updating authenticationoauthidpprofile %s", authenticationoauthidpprofileName)
+		}
+	}
+	return readAuthenticationoauthidpprofileFunc(d, meta)
+}
+
+func deleteAuthenticationoauthidpprofileFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteAuthenticationoauthidpprofileFunc")
+	client := meta.(*NetScalerNitroClient).client
+	authenticationoauthidpprofileName := d.Id()
+	err := client.DeleteResource("authenticationoauthidpprofile", authenticationoauthidpprofileName)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
---
+++ citrixadc/resource_citrixadc_authenticationoauthidpprofile_test.go
@@ -0,0 +1,124 @@
+/*
+Copyright 2016 Citrix Systems, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package citrixadc
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"testing"
+)
+
+const testAccAuthenticationoauthidpprofile_add = `
+
+	resource "citrixadc_authenticationoauthidpprofile" "tf_idpprofile" {
+		name         = "tf_idpprofile"
+		clientid     = "cliId"
+		clientsecret = "secret"
+		redirecturl  = "http://www.example.com/1/"
+	}
+`
+const testAccAuthenticationoauthidpprofile_update = `
+
+	resource "citrixadc_authenticationoauthidpprofile" "tf_idpprofile" {
+		name          = "tf_idpprofile"
+		clientid      = "cliId1"
+		clientsecret  = "secret"
+		redirecturl   = "http://www.example11.com/1/"
+	}
+`
+
+func TestAccAuthenticationoauthidpprofile_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAuthenticationoauthidpprofileDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccAuthenticationoauthidpprofile_add,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAuthenticationoauthidpprofileExist("citrixadc_authenticationoauthidpprofile.tf_idpprofile", nil),
+					resource.TestCheckResourceAttr("citrixadc_authenticationoauthidpprofile.tf_idpprofile", "name", "tf_idpprofile"),
+					resource.TestCheckResourceAttr("citrixadc_authenticationoauthidpprofile.tf_idpprofile", "clientid", "cliId"),
+				),
+			},
+			resource.TestStep{
+				Config: testAccAuthenticationoauthidpprofile_update,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAuthenticationoauthidpprofileExist("citrixadc_authenticationoauthidpprofile.tf_idpprofile", nil),
+					resource.TestCheckResourceAttr("citrixadc_authenticationoauthidpprofile.tf_idpprofile", "name", "tf_idpprofile"),
+					resource.TestCheckResourceAttr("citrixadc_authenticationoauthidpprofile.tf_idpprofile", "clientid", "cliId1"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAuthenticationoauthidpprofileExist(n string, id *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No authenticationoauthidpprofile name is set")
+		}
+
+		if id != nil {
+			if *id != "" && *id != rs.Primary.ID {
+				return fmt.Errorf("Resource ID has changed!")
+			}
+
+			*id = rs.Primary.ID
+		}
+
+		nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+		data, err := nsClient.FindResource("authenticationoauthidpprofile", rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		if data == nil {
+			return fmt.Errorf("authenticationoauthidpprofile %s not found", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckAuthenticationoauthidpprofileDestroy(s *terraform.State) error {
+	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "citrixadc_authenticationoauthidpprofile" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No name is set")
+		}
+
+		_, err := nsClient.FindResource("authenticationoauthidpprofile", rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("authenticationoauthidpprofile %s still exists", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
