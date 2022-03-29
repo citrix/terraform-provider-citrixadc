d0c81b341a36cda8969b1db13e87152eb2739a37
Rohit Myali <rohit.myali@citrix.com>
2022-03-29 04:21:01 +0000

    Added vpnvserver_authenticationtacacspolicy_binding Resource

--- citrixadc/provider.go
+++ citrixadc/provider.go
@@ -380,6 +380,7 @@ func providerResources() map[string]*schema.Resource {
 		"citrixadc_vpnvserver_authenticationcertpolicy_binding":            resourceCitrixAdcVpnvserver_authenticationcertpolicy_binding(),
 		"citrixadc_vpnvserver_authenticationlocalpolicy_binding":           resourceCitrixAdcVpnvserver_authenticationlocalpolicy_binding(),
 		"citrixadc_vpnvserver_authenticationsamlpolicy_binding":            resourceCitrixAdcVpnvserver_authenticationsamlpolicy_binding(),
+		"citrixadc_vpnvserver_authenticationtacacspolicy_binding":          resourceCitrixAdcVpnvserver_authenticationtacacspolicy_binding(),
 	}
 }
 
