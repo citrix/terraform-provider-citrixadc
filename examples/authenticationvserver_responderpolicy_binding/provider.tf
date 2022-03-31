0883481ce8d31f7dc6c4ca5580f0ddad9a61b065
Rohit Myali <rohit.myali@citrix.com>
2022-03-31 07:41:31 +0000

    Added authenticationvserver_responderpolicy_binding Resource

---
+++ examples/authenticationvserver_responderpolicy_binding/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    citrixadc = {
+      source = "citrix/citrixadc"
+    }
+  }
+}
+provider "citrixadc" {
+  endpoint = "http://localhost:8080"
+  username = "<username>"
+  password = "<password>"
+}
+
