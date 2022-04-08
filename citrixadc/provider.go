30c088c593b593f917d31c314bc5b5eec0b0677e
Rohit Myali <rohit.myali@citrix.com>
2022-04-08 11:59:10 +0000

    Added vpnvserver_intranetip6_binding Resource

--- citrixadc/provider.go
+++ citrixadc/provider.go
@@ -422,6 +422,7 @@ func providerResources() map[string]*schema.Resource {
 		"citrixadc_vpnglobal_staserver_binding":                                   resourceCitrixAdcVpnglobal_staserver_binding(),
 		"citrixadc_vpnglobal_authenticationnegotiatepolicy_binding":               resourceCitrixAdcVpnglobal_authenticationnegotiatepolicy_binding(),
 		"citrixadc_vpnglobal_intranetip6_binding":                                 resourceCitrixAdcVpnglobal_intranetip6_binding(),
+		"citrixadc_vpnvserver_intranetip6_binding":                                resourceCitrixAdcVpnvserver_intranetip6_binding(),
 	}
 }
 
