694ac0a6f1b7ff13c198c5c5aa0854caf9cf47f9
Rohit Myali <rohit.myali@citrix.com>
2022-04-22 05:49:30 +0000

    Added appfwprofile_contenttype_binding Resource

---
+++ citrixadc/resource_citrixadc_appfwprofile_contenttype_binding.go
@@ -0,0 +1,197 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/appfw"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+	"strings"
+	"net/url"
+)
+
+func resourceCitrixAdcAppfwprofile_contenttype_binding() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createAppfwprofile_contenttype_bindingFunc,
+		Read:          readAppfwprofile_contenttype_bindingFunc,
+		Delete:        deleteAppfwprofile_contenttype_bindingFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				Computed: false,
+				ForceNew: true,
+			},
+			"contenttype": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				Computed: false,
+				ForceNew: true,
+			},
+			"alertonly": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"isautodeployed": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"resourceid": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"ruletype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"state": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func createAppfwprofile_contenttype_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createAppfwprofile_contenttype_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	name := d.Get("name")
+	contenttype := d.Get("contenttype")
+	bindingId := fmt.Sprintf("%s,%s", name, contenttype)
+	appfwprofile_contenttype_binding := appfw.Appfwprofilecontenttypebinding{
+		Alertonly:      d.Get("alertonly").(string),
+		Comment:        d.Get("comment").(string),
+		Contenttype:    d.Get("contenttype").(string),
+		Isautodeployed: d.Get("isautodeployed").(string),
+		Name:           d.Get("name").(string),
+		Resourceid:     d.Get("resourceid").(string),
+		Ruletype:       d.Get("ruletype").(string),
+		State:          d.Get("state").(string),
+	}
+
+	err := client.UpdateUnnamedResource(service.Appfwprofile_contenttype_binding.Type(), &appfwprofile_contenttype_binding)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(bindingId)
+
+	err = readAppfwprofile_contenttype_bindingFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this appfwprofile_contenttype_binding but we can't read it ?? %s", bindingId)
+		return nil
+	}
+	return nil
+}
+
+func readAppfwprofile_contenttype_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readAppfwprofile_contenttype_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	name := idSlice[0]
+	contenttype := idSlice[1]
+
+	log.Printf("[DEBUG] citrixadc-provider: Reading appfwprofile_contenttype_binding state %s", bindingId)
+
+	findParams := service.FindParams{
+		ResourceType:             "appfwprofile_contenttype_binding",
+		ResourceName:             name,
+		ResourceMissingErrorCode: 258,
+	}
+	dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+	// Unexpected error
+	if err != nil {
+		log.Printf("[DEBUG] citrixadc-provider: Error during FindResourceArrayWithParams %s", err.Error())
+		return err
+	}
+
+	// Resource is missing
+	if len(dataArr) == 0 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams returned empty array")
+		log.Printf("[WARN] citrixadc-provider: Clearing appfwprofile_contenttype_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+
+	// Iterate through results to find the one with the right id
+	foundIndex := -1
+	for i, v := range dataArr {
+		if v["contenttype"].(string) == contenttype {
+			foundIndex = i
+			break
+		}
+	}
+
+	// Resource is missing
+	if foundIndex == -1 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams secondIdComponent not found in array")
+		log.Printf("[WARN] citrixadc-provider: Clearing appfwprofile_contenttype_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+	// Fallthrough
+
+	data := dataArr[foundIndex]
+
+	d.Set("alertonly", data["alertonly"])
+	d.Set("comment", data["comment"])
+	d.Set("contenttype", data["contenttype"])
+	d.Set("isautodeployed", data["isautodeployed"])
+	d.Set("name", data["name"])
+	d.Set("resourceid", data["resourceid"])
+	d.Set("ruletype", data["ruletype"])
+	d.Set("state", data["state"])
+
+	return nil
+
+}
+
+func deleteAppfwprofile_contenttype_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteAppfwprofile_contenttype_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	name := idSlice[0]
+	contenttype := idSlice[1]
+
+	args := make([]string, 0)
+	args = append(args, fmt.Sprintf("contenttype:%s", contenttype))
+	if val, ok := d.GetOk("ruletype"); ok {
+		args = append(args, fmt.Sprintf("ruletype:%s", url.QueryEscape(val.(string))))
+	}
+
+	err := client.DeleteResourceWithArgs(service.Appfwprofile_contenttype_binding.Type(), name, args)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
---
+++ citrixadc/resource_citrixadc_appfwprofile_contenttype_binding_test.go
@@ -0,0 +1,244 @@
+/*
+Copyright 2016 Citrix Systems, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package citrixadc
+
+import (
+	"fmt"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"strings"
+	"testing"
+)
+
+const testAccAppfwprofile_contenttype_binding_basic = `
+	resource "citrixadc_appfwprofile" "tf_appfwprofile" {
+		name                     = "tf_appfwprofile"
+		bufferoverflowaction     = ["none"]
+		contenttypeaction        = ["none"]
+		cookieconsistencyaction  = ["none"]
+		creditcard               = ["none"]
+		creditcardaction         = ["none"]
+		crosssitescriptingaction = ["none"]
+		csrftagaction            = ["none"]
+		denyurlaction            = ["none"]
+		dynamiclearning          = ["none"]
+		fieldconsistencyaction   = ["none"]
+		fieldformataction        = ["none"]
+		fileuploadtypesaction    = ["none"]
+		inspectcontenttypes      = ["none"]
+		jsondosaction            = ["none"]
+		jsonsqlinjectionaction   = ["none"]
+		jsonxssaction            = ["none"]
+		multipleheaderaction     = ["none"]
+		sqlinjectionaction       = ["none"]
+		starturlaction           = ["none"]
+		type                     = ["HTML"]
+		xmlattachmentaction      = ["none"]
+		xmldosaction             = ["none"]
+		xmlformataction          = ["none"]
+		xmlsoapfaultaction       = ["none"]
+		xmlsqlinjectionaction    = ["none"]
+		xmlvalidationaction      = ["none"]
+		xmlwsiaction             = ["none"]
+		xmlxssaction             = ["none"]
+	}
+	resource "citrixadc_appfwprofile_contenttype_binding" "tf_binding" {
+		name           = citrixadc_appfwprofile.tf_appfwprofile.name
+		contenttype    = "hello"
+		state          = "ENABLED"
+		alertonly      = "ON"
+		isautodeployed = "NOTAUTODEPLOYED"
+		comment        = "Testing"
+	}
+`
+
+const testAccAppfwprofile_contenttype_binding_basic_step2 = `
+	# Keep the above bound resources without the actual binding to check proper deletion
+	resource "citrixadc_appfwprofile" "tf_appfwprofile" {
+		name                     = "tf_appfwprofile"
+		bufferoverflowaction     = ["none"]
+		contenttypeaction        = ["none"]
+		cookieconsistencyaction  = ["none"]
+		creditcard               = ["none"]
+		creditcardaction         = ["none"]
+		crosssitescriptingaction = ["none"]
+		csrftagaction            = ["none"]
+		denyurlaction            = ["none"]
+		dynamiclearning          = ["none"]
+		fieldconsistencyaction   = ["none"]
+		fieldformataction        = ["none"]
+		fileuploadtypesaction    = ["none"]
+		inspectcontenttypes      = ["none"]
+		jsondosaction            = ["none"]
+		jsonsqlinjectionaction   = ["none"]
+		jsonxssaction            = ["none"]
+		multipleheaderaction     = ["none"]
+		sqlinjectionaction       = ["none"]
+		starturlaction           = ["none"]
+		type                     = ["HTML"]
+		xmlattachmentaction      = ["none"]
+		xmldosaction             = ["none"]
+		xmlformataction          = ["none"]
+		xmlsoapfaultaction       = ["none"]
+		xmlsqlinjectionaction    = ["none"]
+		xmlvalidationaction      = ["none"]
+		xmlwsiaction             = ["none"]
+		xmlxssaction             = ["none"]
+	}
+`
+
+func TestAccAppfwprofile_contenttype_binding_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAppfwprofile_contenttype_bindingDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccAppfwprofile_contenttype_binding_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAppfwprofile_contenttype_bindingExist("citrixadc_appfwprofile_contenttype_binding.tf_binding", nil),
+				),
+			},
+			resource.TestStep{
+				Config: testAccAppfwprofile_contenttype_binding_basic_step2,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAppfwprofile_contenttype_bindingNotExist("citrixadc_appfwprofile_contenttype_binding.tf_binding", "tf_appfwprofile,hello"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckAppfwprofile_contenttype_bindingExist(n string, id *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No appfwprofile_contenttype_binding id is set")
+		}
+
+		if id != nil {
+			if *id != "" && *id != rs.Primary.ID {
+				return fmt.Errorf("Resource ID has changed!")
+			}
+
+			*id = rs.Primary.ID
+		}
+
+		client := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+		bindingId := rs.Primary.ID
+
+		idSlice := strings.SplitN(bindingId, ",", 2)
+
+		name := idSlice[0]
+		contenttype := idSlice[1]
+
+		findParams := service.FindParams{
+			ResourceType:             "appfwprofile_contenttype_binding",
+			ResourceName:             name,
+			ResourceMissingErrorCode: 258,
+		}
+		dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+		// Unexpected error
+		if err != nil {
+			return err
+		}
+
+		// Iterate through results to find the one with the matching secondIdComponent
+		found := false
+		for _, v := range dataArr {
+			if v["contenttype"].(string) == contenttype {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return fmt.Errorf("appfwprofile_contenttype_binding %s not found", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckAppfwprofile_contenttype_bindingNotExist(n string, id string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+		if !strings.Contains(id, ",") {
+			return fmt.Errorf("Invalid id string %v. The id string must contain a comma.", id)
+		}
+		idSlice := strings.SplitN(id, ",", 2)
+
+		name := idSlice[0]
+		contenttype := idSlice[1]
+
+		findParams := service.FindParams{
+			ResourceType:             "appfwprofile_contenttype_binding",
+			ResourceName:             name,
+			ResourceMissingErrorCode: 258,
+		}
+		dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+		// Unexpected error
+		if err != nil {
+			return err
+		}
+
+		// Iterate through results to hopefully not find the one with the matching secondIdComponent
+		found := false
+		for _, v := range dataArr {
+			if v["contenttype"].(string) == contenttype {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			return fmt.Errorf("appfwprofile_contenttype_binding %s was found, but it should have been destroyed", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckAppfwprofile_contenttype_bindingDestroy(s *terraform.State) error {
+	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "citrixadc_appfwprofile_contenttype_binding" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No name is set")
+		}
+
+		_, err := nsClient.FindResource(service.Appfwprofile_contenttype_binding.Type(), rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("appfwprofile_contenttype_binding %s still exists", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
