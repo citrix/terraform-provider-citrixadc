210f714ef6b54813fbf9334324c786a9b05ca419
Rohit Myali <rohit.myali@citrix.com>
2022-04-28 11:24:42 +0000

    Added appfwprofile_fileuploadtype_binding Resource

---
+++ examples/appfwprofile_fileuploadtype_binding/resources.tf
@@ -0,0 +1,37 @@
+resource "citrixadc_appfwprofile" "tf_appfwprofile" {
+  name                     = "tf_appfwprofile"
+  bufferoverflowaction     = ["none"]
+  contenttypeaction        = ["none"]
+  cookieconsistencyaction  = ["none"]
+  creditcard               = ["none"]
+  creditcardaction         = ["none"]
+  crosssitescriptingaction = ["none"]
+  csrftagaction            = ["none"]
+  denyurlaction            = ["none"]
+  dynamiclearning          = ["none"]
+  fieldconsistencyaction   = ["none"]
+  fieldformataction        = ["none"]
+  fileuploadtypesaction    = ["none"]
+  inspectcontenttypes      = ["none"]
+  jsondosaction            = ["none"]
+  jsonsqlinjectionaction   = ["none"]
+  jsonxssaction            = ["none"]
+  multipleheaderaction     = ["none"]
+  sqlinjectionaction       = ["none"]
+  starturlaction           = ["none"]
+  type                     = ["HTML"]
+  xmlattachmentaction      = ["none"]
+  xmldosaction             = ["none"]
+  xmlformataction          = ["none"]
+  xmlsoapfaultaction       = ["none"]
+  xmlsqlinjectionaction    = ["none"]
+  xmlvalidationaction      = ["none"]
+  xmlwsiaction             = ["none"]
+  xmlxssaction             = ["none"]
+}
+resource "citrixadc_appfwprofile_fileuploadtype_binding" "tf_binding" {
+  name                   = citrixadc_appfwprofile.tf_appfwprofile.name
+  fileuploadtype         = "tf_uploadtype"
+  as_fileuploadtypes_url = "www.example.com"
+  filetype               = ["pdf", "text"]
+}
