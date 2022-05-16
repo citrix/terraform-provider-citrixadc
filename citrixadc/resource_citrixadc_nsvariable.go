f766c1c7de87f80e7fb148b1efba19413942911b
Rohit Myali <rohit.myali@citrix.com>
2022-05-16 06:17:27 +0000

    Added nsvariable Resource

---
+++ citrixadc/resource_citrixadc_nsvariable.go
@@ -0,0 +1,193 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/ns"
+
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+)
+
+func resourceCitrixAdcNsvariable() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createNsvariableFunc,
+		Read:          readNsvariableFunc,
+		Update:        updateNsvariableFunc,
+		Delete:        deleteNsvariableFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				Computed: false,
+				ForceNew: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				Computed: false,
+				ForceNew: true,
+			},
+			"scope": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"expires": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"iffull": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ifnovalue": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ifvaluetoobig": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"init": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func createNsvariableFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createNsvariableFunc")
+	client := meta.(*NetScalerNitroClient).client
+	nsvariableName := d.Get("name").(string)
+	nsvariable := ns.Nsvariable{
+		Comment:       d.Get("comment").(string),
+		Expires:       d.Get("expires").(int),
+		Iffull:        d.Get("iffull").(string),
+		Ifnovalue:     d.Get("ifnovalue").(string),
+		Ifvaluetoobig: d.Get("ifvaluetoobig").(string),
+		Init:          d.Get("init").(string),
+		Name:          d.Get("name").(string),
+		Scope:         d.Get("scope").(string),
+		Type:          d.Get("type").(string),
+	}
+
+	_, err := client.AddResource(service.Nsvariable.Type(), nsvariableName, &nsvariable)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(nsvariableName)
+
+	err = readNsvariableFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this nsvariable but we can't read it ?? %s", nsvariableName)
+		return nil
+	}
+	return nil
+}
+
+func readNsvariableFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readNsvariableFunc")
+	client := meta.(*NetScalerNitroClient).client
+	nsvariableName := d.Id()
+	log.Printf("[DEBUG] citrixadc-provider: Reading nsvariable state %s", nsvariableName)
+	data, err := client.FindResource(service.Nsvariable.Type(), nsvariableName)
+	if err != nil {
+		log.Printf("[WARN] citrixadc-provider: Clearing nsvariable state %s", nsvariableName)
+		d.SetId("")
+		return nil
+	}
+	d.Set("comment", data["comment"])
+	d.Set("expires", data["expires"])
+	d.Set("iffull", data["iffull"])
+	d.Set("ifnovalue", data["ifnovalue"])
+	d.Set("ifvaluetoobig", data["ifvaluetoobig"])
+	d.Set("init", data["init"])
+	d.Set("name", data["name"])
+	d.Set("scope", data["scope"])
+	d.Set("type", data["type"])
+
+	return nil
+
+}
+
+func updateNsvariableFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In updateNsvariableFunc")
+	client := meta.(*NetScalerNitroClient).client
+	nsvariableName := d.Get("name").(string)
+
+	nsvariable := ns.Nsvariable{
+		Name: d.Get("name").(string),
+	}
+	hasChange := false
+	if d.HasChange("comment") {
+		log.Printf("[DEBUG]  citrixadc-provider: Comment has changed for nsvariable %s, starting update", nsvariableName)
+		nsvariable.Comment = d.Get("comment").(string)
+		hasChange = true
+	}
+	if d.HasChange("expires") {
+		log.Printf("[DEBUG]  citrixadc-provider: Expires has changed for nsvariable %s, starting update", nsvariableName)
+		nsvariable.Expires = d.Get("expires").(int)
+		hasChange = true
+	}
+	if d.HasChange("iffull") {
+		log.Printf("[DEBUG]  citrixadc-provider: Iffull has changed for nsvariable %s, starting update", nsvariableName)
+		nsvariable.Iffull = d.Get("iffull").(string)
+		hasChange = true
+	}
+	if d.HasChange("ifnovalue") {
+		log.Printf("[DEBUG]  citrixadc-provider: Ifnovalue has changed for nsvariable %s, starting update", nsvariableName)
+		nsvariable.Ifnovalue = d.Get("ifnovalue").(string)
+		hasChange = true
+	}
+	if d.HasChange("ifvaluetoobig") {
+		log.Printf("[DEBUG]  citrixadc-provider: Ifvaluetoobig has changed for nsvariable %s, starting update", nsvariableName)
+		nsvariable.Ifvaluetoobig = d.Get("ifvaluetoobig").(string)
+		hasChange = true
+	}
+	if d.HasChange("init") {
+		log.Printf("[DEBUG]  citrixadc-provider: Init has changed for nsvariable %s, starting update", nsvariableName)
+		nsvariable.Init = d.Get("init").(string)
+		hasChange = true
+	}
+
+	if hasChange {
+		_, err := client.UpdateResource(service.Nsvariable.Type(), nsvariableName, &nsvariable)
+		if err != nil {
+			return fmt.Errorf("Error updating nsvariable %s", nsvariableName)
+		}
+	}
+	return readNsvariableFunc(d, meta)
+}
+
+func deleteNsvariableFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteNsvariableFunc")
+	client := meta.(*NetScalerNitroClient).client
+	nsvariableName := d.Id()
+	err := client.DeleteResource(service.Nsvariable.Type(), nsvariableName)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
---
+++ citrixadc/resource_citrixadc_nsvariable_test.go
@@ -0,0 +1,133 @@
+/*
+Copyright 2016 Citrix Systems, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package citrixadc
+
+import (
+	"fmt"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"testing"
+)
+
+const testAccNsvariable_add = `
+	resource "citrixadc_nsvariable" "tf_nsvariable" {
+		name          = "tf_nsvariable"
+		type          = "text(20)"
+		scope         = "global"
+		iffull        = "undef"
+		ifvaluetoobig = "undef"
+		ifnovalue     = "undef"
+		comment       = "Testing"
+	}
+`
+const testAccNsvariable_update = `
+	resource "citrixadc_nsvariable" "tf_nsvariable" {
+		name          = "tf_nsvariable"
+		type          = "text(20)"
+		scope         = "global"
+		iffull        = "lru"
+		ifvaluetoobig = "truncate"
+		ifnovalue     = "init"
+		comment       = "Testing"
+	}
+`
+
+func TestAccNsvariable_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckNsvariableDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccNsvariable_add,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsvariableExist("citrixadc_nsvariable.tf_nsvariable", nil),
+					resource.TestCheckResourceAttr("citrixadc_nsvariable.tf_nsvariable", "name", "tf_nsvariable"),
+					resource.TestCheckResourceAttr("citrixadc_nsvariable.tf_nsvariable", "iffull", "undef"),
+					resource.TestCheckResourceAttr("citrixadc_nsvariable.tf_nsvariable", "ifvaluetoobig", "undef"),
+					resource.TestCheckResourceAttr("citrixadc_nsvariable.tf_nsvariable", "ifnovalue", "undef"),
+				),
+			},
+			resource.TestStep{
+				Config: testAccNsvariable_update,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckNsvariableExist("citrixadc_nsvariable.tf_nsvariable", nil),
+					resource.TestCheckResourceAttr("citrixadc_nsvariable.tf_nsvariable", "name", "tf_nsvariable"),
+					resource.TestCheckResourceAttr("citrixadc_nsvariable.tf_nsvariable", "iffull", "lru"),
+					resource.TestCheckResourceAttr("citrixadc_nsvariable.tf_nsvariable", "ifvaluetoobig", "truncate"),
+					resource.TestCheckResourceAttr("citrixadc_nsvariable.tf_nsvariable", "ifnovalue", "init"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckNsvariableExist(n string, id *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No nsvariable name is set")
+		}
+
+		if id != nil {
+			if *id != "" && *id != rs.Primary.ID {
+				return fmt.Errorf("Resource ID has changed!")
+			}
+
+			*id = rs.Primary.ID
+		}
+
+		nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+		data, err := nsClient.FindResource(service.Nsvariable.Type(), rs.Primary.ID)
+
+		if err != nil {
+			return err
+		}
+
+		if data == nil {
+			return fmt.Errorf("nsvariable %s not found", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckNsvariableDestroy(s *terraform.State) error {
+	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "citrixadc_nsvariable" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No name is set")
+		}
+
+		_, err := nsClient.FindResource(service.Nsvariable.Type(), rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("nsvariable %s still exists", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
