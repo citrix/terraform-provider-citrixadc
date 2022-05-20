5d1dbb2efe705fdd076ecdc50020e46646a4a501
Rohit Myali <rohit.myali@citrix.com>
2022-05-20 06:54:57 +0000

    Added nspartition_vlan_binding Resource

---
+++ citrixadc/resource_citrixadc_nspartition_vlan_binding.go
@@ -0,0 +1,146 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/ns"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+	"strings"
+	"strconv"
+)
+
+func resourceCitrixAdcNspartition_vlan_binding() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createNspartition_vlan_bindingFunc,
+		Read:          readNspartition_vlan_bindingFunc,
+		Delete:        deleteNspartition_vlan_bindingFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"partitionname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				Computed: false,
+				ForceNew: true,
+			},
+			"vlan": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+				Computed: false,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func createNspartition_vlan_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createNspartition_vlan_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	partitionname := d.Get("partitionname")
+	vlan := strconv.Itoa(d.Get("vlan").(int))
+	bindingId := fmt.Sprintf("%s,%s", partitionname, vlan)
+	nspartition_vlan_binding := ns.Nspartitionvlanbinding{
+		Partitionname: d.Get("partitionname").(string),
+		Vlan:          d.Get("vlan").(int),
+	}
+
+	err := client.UpdateUnnamedResource(service.Nspartition_vlan_binding.Type(), &nspartition_vlan_binding)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(bindingId)
+
+	err = readNspartition_vlan_bindingFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this nspartition_vlan_binding but we can't read it ?? %s", bindingId)
+		return nil
+	}
+	return nil
+}
+
+func readNspartition_vlan_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readNspartition_vlan_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	partitionname := idSlice[0]
+	vlan := idSlice[1]
+
+	log.Printf("[DEBUG] citrixadc-provider: Reading nspartition_vlan_binding state %s", bindingId)
+
+	findParams := service.FindParams{
+		ResourceType:             "nspartition_vlan_binding",
+		ResourceName:             partitionname,
+		ResourceMissingErrorCode: 258,
+	}
+	dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+	// Unexpected error
+	if err != nil {
+		log.Printf("[DEBUG] citrixadc-provider: Error during FindResourceArrayWithParams %s", err.Error())
+		return err
+	}
+
+	// Resource is missing
+	if len(dataArr) == 0 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams returned empty array")
+		log.Printf("[WARN] citrixadc-provider: Clearing nspartition_vlan_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+
+	// Iterate through results to find the one with the right id
+	foundIndex := -1
+	for i, v := range dataArr {
+		if v["vlan"].(string) == vlan {
+			foundIndex = i
+			break
+		}
+	}
+
+	// Resource is missing
+	if foundIndex == -1 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams secondIdComponent not found in array")
+		log.Printf("[WARN] citrixadc-provider: Clearing nspartition_vlan_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+	// Fallthrough
+
+	data := dataArr[foundIndex]
+
+	d.Set("partitionname", data["partitionname"])
+	d.Set("vlan", data["vlan"])
+
+	return nil
+
+}
+
+func deleteNspartition_vlan_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteNspartition_vlan_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	name := idSlice[0]
+	vlan := idSlice[1]
+
+	args := make([]string, 0)
+	args = append(args, fmt.Sprintf("vlan:%s", vlan))
+
+	err := client.DeleteResourceWithArgs(service.Nspartition_vlan_binding.Type(), name, args)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
