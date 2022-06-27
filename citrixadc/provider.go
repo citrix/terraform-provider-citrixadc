9069dc7958f0c2b06e5ea2a8cad893dcb4c01c8c
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2022-06-27 15:57:31 +0530

    Dnsview resource added (#589)
    dnsview resource added

--- citrixadc/provider.go
+++ citrixadc/provider.go
@@ -551,6 +551,7 @@ func providerResources() map[string]*schema.Resource {
 		"citrixadc_mapdomain_mapbmr_binding":                                      resourceCitrixAdcMapdomain_mapbmr_binding(),
 		"citrixadc_netbridge_vlan_binding":                                        resourceCitrixAdcNetbridge_vlan_binding(),
 		"citrixadc_bridgetable":                                                   resourceCitrixAdcBridgetable(),
+		"citrixadc_dnsview":													   resourceCitrixAdcDnsview(),
 	}
 }
 
