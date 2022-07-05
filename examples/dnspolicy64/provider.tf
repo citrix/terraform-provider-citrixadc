09b1f0e494042d6db2ba1efdd44fed47ac9bf500
Shivashankar Vaddepally <shivashankar.vaddepally@citrix.com>
2022-07-05 07:00:23 +0000

    Add resource Dns policy64

---
+++ examples/dnspolicy64/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    citrixadc = {
+      source = "citrix/citrixadc"
+    }
+  }
+}
+provider "citrixadc" {
+  endpoint = "http://localhost:8000"
+  username = "<username"
+  password = "<password>"
+}
