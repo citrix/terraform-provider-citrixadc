2aaf298580c1728e49bdac61b89fdbaf085431ec
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2022-07-07 15:03:10 +0530

    added resource dnsnaptrrec (#602)
    Signed-off-by: Sumanth Lingappa <sumanth.lingappa@citrix.com>
    
    Co-authored-by: Shivashankar Vaddepally <shivashankar.vaddepally@citrix.com>

--- citrixadc/provider.go
+++ citrixadc/provider.go
@@ -552,6 +552,8 @@ func providerResources() map[string]*schema.Resource {
 		"citrixadc_mapdomain_mapbmr_binding":                                      resourceCitrixAdcMapdomain_mapbmr_binding(),
 		"citrixadc_netbridge_vlan_binding":                                        resourceCitrixAdcNetbridge_vlan_binding(),
 		"citrixadc_bridgetable":                                                   resourceCitrixAdcBridgetable(),
+		"citrixadc_dnsview":								                          					   resourceCitrixAdcDnsview(),
+		"citrixadc_dnsnaptrrec":								                        				   resourceCitrixAdcDnsnaptrrec(),
 		"citrixadc_dnsview":													                             resourceCitrixAdcDnsview(),
 		"citrixadc_dnscnamerec":												                           resourceCitrixAdcDnscnamerec(),					
 		"citrixadc_dnsview":													                             resourceCitrixAdcDnsview(),
