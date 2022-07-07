663795aac53596133cefdb3dfe278b65951d1b91
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2022-07-07 15:28:42 +0530

    added resource dnspolicy (#607)
    Co-authored-by: Shivashankar Vaddepally <shivashankar.vaddepally@citrix.com>

--- citrixadc/provider.go
+++ citrixadc/provider.go
@@ -548,6 +548,7 @@ func providerResources() map[string]*schema.Resource {
 		"citrixadc_rsskeytype":                                                    resourceCitrixAdcRsskeytype(),
 		"citrixadc_nat64":                                                         resourceCitrixAdcNat64(),
 		"citrixadc_netbridge_iptunnel_binding":                                    resourceCitrixAdcNetbridge_iptunnel_binding(),
+		"citrixadc_dnspolicy":												                        	   resourceCitrixAdcDnspolicy(),
 		"citrixadc_dnsaction":                                                     resourceCitrixAdcDnsaction(),
 		"citrixadc_dnsprofile":                                                    resourceCitrixAdcDnsprofile(),
 		"citrixadc_mapdomain_mapbmr_binding":                                      resourceCitrixAdcMapdomain_mapbmr_binding(),
