46698fd6fceffddda9efad149c1f798126b262c3
christosm <christos.mersinis@citrix.com>
2022-07-12 14:56:18 +0000

    add gslbservicegroup

---
+++ citrixadc/resource_citrixadc_gslbservicegroup.go
@@ -0,0 +1,454 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/gslb"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+)
+
+func resourceCitrixAdcGslbservicegroup() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createGslbservicegroupFunc,
+		Read:          readGslbservicegroupFunc,
+		Update:        updateGslbservicegroupFunc,
+		Delete:        deleteGslbservicegroupFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"servicegroupname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"appflowlog": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"autoscale": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"cip": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"cipheader": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"clttimeout": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"comment": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"delay": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"downstateflush": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dupweight": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"graceful": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"hashid": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"healthmonitor": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"includemembers": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+			},
+			"maxbandwidth": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"maxclient": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"monitornamesvc": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"monthreshold": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"newname": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"port": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"publicip": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"publicport": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"servername": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"servicetype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"sitename": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"sitepersistence": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"siteprefix": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"state": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"svrtimeout": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"weight": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func createGslbservicegroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createGslbservicegroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	var gslbservicegroupName string
+	if v, ok := d.GetOk("servicegroupname"); ok {
+		gslbservicegroupName = v.(string)
+	} else {
+		gslbservicegroupName = resource.PrefixedUniqueId("tf-gslbservicegroup-")
+		d.Set("servicegroupname", gslbservicegroupName)
+	}
+	gslbservicegroup := gslb.Gslbservicegroup{
+		Appflowlog:       d.Get("appflowlog").(string),
+		Autoscale:        d.Get("autoscale").(string),
+		Cip:              d.Get("cip").(string),
+		Cipheader:        d.Get("cipheader").(string),
+		Clttimeout:       d.Get("clttimeout").(int),
+		Comment:          d.Get("comment").(string),
+		Delay:            d.Get("delay").(int),
+		Downstateflush:   d.Get("downstateflush").(string),
+		Dupweight:        d.Get("dupweight").(int),
+		Graceful:         d.Get("graceful").(string),
+		Hashid:           d.Get("hashid").(int),
+		Healthmonitor:    d.Get("healthmonitor").(string),
+		Includemembers:   d.Get("includemembers").(bool),
+		Maxbandwidth:     d.Get("maxbandwidth").(int),
+		Maxclient:        d.Get("maxclient").(int),
+		Monitornamesvc:   d.Get("monitornamesvc").(string),
+		Monthreshold:     d.Get("monthreshold").(int),
+		Newname:          d.Get("newname").(string),
+		Port:             d.Get("port").(int),
+		Publicip:         d.Get("publicip").(string),
+		Publicport:       d.Get("publicport").(int),
+		Servername:       d.Get("servername").(string),
+		Servicegroupname: d.Get("servicegroupname").(string),
+		Servicetype:      d.Get("servicetype").(string),
+		Sitename:         d.Get("sitename").(string),
+		Sitepersistence:  d.Get("sitepersistence").(string),
+		Siteprefix:       d.Get("siteprefix").(string),
+		State:            d.Get("state").(string),
+		Svrtimeout:       d.Get("svrtimeout").(int),
+		Weight:           d.Get("weight").(int),
+	}
+
+	_, err := client.AddResource("gslbservicegroup", gslbservicegroupName, &gslbservicegroup)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(gslbservicegroupName)
+
+	err = readGslbservicegroupFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this gslbservicegroup but we can't read it ?? %s", gslbservicegroupName)
+		return nil
+	}
+	return nil
+}
+
+func readGslbservicegroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readGslbservicegroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	gslbservicegroupName := d.Id()
+	log.Printf("[DEBUG] citrixadc-provider: Reading gslbservicegroup state %s", gslbservicegroupName)
+	data, err := client.FindResource("gslbservicegroup", gslbservicegroupName)
+	if err != nil {
+		log.Printf("[WARN] citrixadc-provider: Clearing gslbservicegroup state %s", gslbservicegroupName)
+		d.SetId("")
+		return nil
+	}
+	d.Set("servicegroupname", data["servicegroupname"])
+	d.Set("appflowlog", data["appflowlog"])
+	d.Set("autoscale", data["autoscale"])
+	d.Set("cip", data["cip"])
+	d.Set("cipheader", data["cipheader"])
+	d.Set("clttimeout", data["clttimeout"])
+	d.Set("comment", data["comment"])
+	d.Set("delay", data["delay"])
+	d.Set("downstateflush", data["downstateflush"])
+	d.Set("dupweight", data["dupweight"])
+	d.Set("graceful", data["graceful"])
+	d.Set("hashid", data["hashid"])
+	d.Set("healthmonitor", data["healthmonitor"])
+	d.Set("includemembers", data["includemembers"])
+	d.Set("maxbandwidth", data["maxbandwidth"])
+	d.Set("maxclient", data["maxclient"])
+	d.Set("monitornamesvc", data["monitornamesvc"])
+	d.Set("monthreshold", data["monthreshold"])
+	d.Set("newname", data["newname"])
+	d.Set("port", data["port"])
+	d.Set("publicip", data["publicip"])
+	d.Set("publicport", data["publicport"])
+	d.Set("servername", data["servername"])
+	d.Set("servicegroupname", data["servicegroupname"])
+	d.Set("servicetype", data["servicetype"])
+	d.Set("sitename", data["sitename"])
+	d.Set("sitepersistence", data["sitepersistence"])
+	d.Set("siteprefix", data["siteprefix"])
+	d.Set("state", data["state"])
+	d.Set("svrtimeout", data["svrtimeout"])
+	d.Set("weight", data["weight"])
+
+	return nil
+
+}
+
+func updateGslbservicegroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In updateGslbservicegroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	gslbservicegroupName := d.Get("servicegroupname").(string)
+
+	gslbservicegroup := gslb.Gslbservicegroup{
+		Servicegroupname: d.Get("servicegroupname").(string),
+	}
+	hasChange := false
+	if d.HasChange("appflowlog") {
+		log.Printf("[DEBUG]  citrixadc-provider: Appflowlog has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Appflowlog = d.Get("appflowlog").(string)
+		hasChange = true
+	}
+	if d.HasChange("autoscale") {
+		log.Printf("[DEBUG]  citrixadc-provider: Autoscale has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Autoscale = d.Get("autoscale").(string)
+		hasChange = true
+	}
+	if d.HasChange("cip") {
+		log.Printf("[DEBUG]  citrixadc-provider: Cip has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Cip = d.Get("cip").(string)
+		hasChange = true
+	}
+	if d.HasChange("cipheader") {
+		log.Printf("[DEBUG]  citrixadc-provider: Cipheader has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Cipheader = d.Get("cipheader").(string)
+		hasChange = true
+	}
+	if d.HasChange("clttimeout") {
+		log.Printf("[DEBUG]  citrixadc-provider: Clttimeout has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Clttimeout = d.Get("clttimeout").(int)
+		hasChange = true
+	}
+	if d.HasChange("comment") {
+		log.Printf("[DEBUG]  citrixadc-provider: Comment has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Comment = d.Get("comment").(string)
+		hasChange = true
+	}
+	if d.HasChange("delay") {
+		log.Printf("[DEBUG]  citrixadc-provider: Delay has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Delay = d.Get("delay").(int)
+		hasChange = true
+	}
+	if d.HasChange("downstateflush") {
+		log.Printf("[DEBUG]  citrixadc-provider: Downstateflush has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Downstateflush = d.Get("downstateflush").(string)
+		hasChange = true
+	}
+	if d.HasChange("dupweight") {
+		log.Printf("[DEBUG]  citrixadc-provider: Dupweight has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Dupweight = d.Get("dupweight").(int)
+		hasChange = true
+	}
+	if d.HasChange("graceful") {
+		log.Printf("[DEBUG]  citrixadc-provider: Graceful has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Graceful = d.Get("graceful").(string)
+		hasChange = true
+	}
+	if d.HasChange("hashid") {
+		log.Printf("[DEBUG]  citrixadc-provider: Hashid has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Hashid = d.Get("hashid").(int)
+		hasChange = true
+	}
+	if d.HasChange("healthmonitor") {
+		log.Printf("[DEBUG]  citrixadc-provider: Healthmonitor has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Healthmonitor = d.Get("healthmonitor").(string)
+		hasChange = true
+	}
+	if d.HasChange("includemembers") {
+		log.Printf("[DEBUG]  citrixadc-provider: Includemembers has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Includemembers = d.Get("includemembers").(bool)
+		hasChange = true
+	}
+	if d.HasChange("maxbandwidth") {
+		log.Printf("[DEBUG]  citrixadc-provider: Maxbandwidth has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Maxbandwidth = d.Get("maxbandwidth").(int)
+		hasChange = true
+	}
+	if d.HasChange("maxclient") {
+		log.Printf("[DEBUG]  citrixadc-provider: Maxclient has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Maxclient = d.Get("maxclient").(int)
+		hasChange = true
+	}
+	if d.HasChange("monitornamesvc") {
+		log.Printf("[DEBUG]  citrixadc-provider: Monitornamesvc has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Monitornamesvc = d.Get("monitornamesvc").(string)
+		hasChange = true
+	}
+	if d.HasChange("monthreshold") {
+		log.Printf("[DEBUG]  citrixadc-provider: Monthreshold has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Monthreshold = d.Get("monthreshold").(int)
+		hasChange = true
+	}
+	if d.HasChange("newname") {
+		log.Printf("[DEBUG]  citrixadc-provider: Newname has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Newname = d.Get("newname").(string)
+		hasChange = true
+	}
+	if d.HasChange("port") {
+		log.Printf("[DEBUG]  citrixadc-provider: Port has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Port = d.Get("port").(int)
+		hasChange = true
+	}
+	if d.HasChange("publicip") {
+		log.Printf("[DEBUG]  citrixadc-provider: Publicip has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Publicip = d.Get("publicip").(string)
+		hasChange = true
+	}
+	if d.HasChange("publicport") {
+		log.Printf("[DEBUG]  citrixadc-provider: Publicport has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Publicport = d.Get("publicport").(int)
+		hasChange = true
+	}
+	if d.HasChange("servicetype") {
+		log.Printf("[DEBUG]  citrixadc-provider: Servicetype has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Servicetype = d.Get("servicetype").(string)
+		hasChange = true
+	}
+	if d.HasChange("sitename") {
+		log.Printf("[DEBUG]  citrixadc-provider: Sitename has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Sitename = d.Get("sitename").(string)
+		hasChange = true
+	}
+	if d.HasChange("sitepersistence") {
+		log.Printf("[DEBUG]  citrixadc-provider: Sitepersistence has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Sitepersistence = d.Get("sitepersistence").(string)
+		hasChange = true
+	}
+	if d.HasChange("siteprefix") {
+		log.Printf("[DEBUG]  citrixadc-provider: Siteprefix has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Siteprefix = d.Get("siteprefix").(string)
+		hasChange = true
+	}
+	if d.HasChange("state") {
+		log.Printf("[DEBUG]  citrixadc-provider: State has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.State = d.Get("state").(string)
+		hasChange = true
+	}
+	if d.HasChange("svrtimeout") {
+		log.Printf("[DEBUG]  citrixadc-provider: Svrtimeout has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Svrtimeout = d.Get("svrtimeout").(int)
+		hasChange = true
+	}
+	if d.HasChange("weight") {
+		log.Printf("[DEBUG]  citrixadc-provider: Weight has changed for gslbservicegroup %s, starting update", gslbservicegroupName)
+		gslbservicegroup.Weight = d.Get("weight").(int)
+		hasChange = true
+	}
+
+	if hasChange {
+		_, err := client.UpdateResource("gslbservicegroup", gslbservicegroupName, &gslbservicegroup)
+		if err != nil {
+			return fmt.Errorf("Error updating gslbservicegroup %s", gslbservicegroupName)
+		}
+	}
+	return readGslbservicegroupFunc(d, meta)
+}
+
+func deleteGslbservicegroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteGslbservicegroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	gslbservicegroupName := d.Id()
+	err := client.DeleteResource("gslbservicegroup", gslbservicegroupName)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
---
+++ docs/resources/gslbservicegroup.md
@@ -0,0 +1,74 @@
+---
+subcategory: "GSLB"
+---
+
+# Resource: gslbservicegroup
+
+The gslbservicegroup resource is used to create gslbservicegroup.
+
+
+## Example usage
+
+```hcl
+resource "citrixadc_gslbservicegroup" "tf_gslbservicegroup" {
+  servicegroupname = "test_gslbvservicegroup"
+  servicetype      = "HTTP"
+  cip              = "DISABLED"
+  healthmonitor    = "NO"
+  sitename         = citrixadc_gslbsite.site_local.sitename
+}
+resource "citrixadc_gslbsite" "site_local" {
+  sitename        = "Site-Local"
+  siteipaddress   = "172.31.96.234"
+  sessionexchange = "DISABLED"
+}
+```
+
+
+## Argument Reference
+* `servicegroupname` - (Required) Name of the GSLB service group. Must begin with an ASCII alphabetic or underscore (_) character, and must contain only ASCII alphanumeric, underscore, hash (#), period (.), space, colon (:), at (@), equals (=), and hyphen (-) characters. Can be changed after the name is created.
+* `appflowlog` - (Optional) Enable logging of AppFlow information for the specified GSLB service group.
+* `autoscale` - (Optional) Auto scale option for a GSLB servicegroup
+* `cip` - (Optional) Insert the Client IP header in requests forwarded to the GSLB service.
+* `cipheader` - (Optional) Name of the HTTP header whose value must be set to the IP address of the client. Used with the Client IP parameter. If client IP insertion is enabled, and the client IP header is not specified, the value of Client IP Header parameter or the value set by the set ns config command is used as client's IP header name.
+* `clttimeout` - (Optional) Time, in seconds, after which to terminate an idle client connection.
+* `comment` - (Optional) Any information about the GSLB service group.
+* `delay` - (Optional) The time allowed (in seconds) for a graceful shutdown. During this period, new connections or requests will continue to be sent to this service for clients who already have a persistent session on the system. Connections or requests from fresh or new clients who do not yet have a persistence sessions on the system will not be sent to the service. Instead, they will be load balanced among other available services. After the delay time expires, no new requests or connections will be sent to the service.
+* `downstateflush` - (Optional) Flush all active transactions associated with all the services in the GSLB service group whose state transitions from UP to DOWN. Do not enable this option for applications that must complete their transactions.
+* `dup_weight` - (Optional) weight of the monitor that is bound to GSLB servicegroup.
+* `graceful` - (Optional) Wait for all existing connections to the service to terminate before shutting down the service.
+* `hashid` - (Optional) The hash identifier for the service. This must be unique for each service. This parameter is used by hash based load balancing methods.
+* `healthmonitor` - (Optional) Monitor the health of this GSLB service.Available settings function are as follows: YES - Send probes to check the health of the GSLB service. NO - Do not send probes to check the health of the GSLB service. With the NO option, the appliance shows the service as UP at all times.
+* `includemembers` - (Optional) Display the members of the listed GSLB service groups in addition to their settings. Can be specified when no service group name is provided in the command. In that case, the details displayed for each service group are identical to the details displayed when a service group name is provided, except that bound monitors are not displayed.
+* `maxbandwidth` - (Optional) Maximum bandwidth, in Kbps, allocated for all the services in the GSLB service group.
+* `maxclient` - (Optional) Maximum number of simultaneous open connections for the GSLB service group.
+* `monitor_name_svc` - (Optional) Name of the monitor bound to the GSLB service group. Used to assign a weight to the monitor.
+* `monthreshold` - (Optional) Minimum sum of weights of the monitors that are bound to this GSLB service. Used to determine whether to mark a GSLB service as UP or DOWN.
+* `newname` - (Optional) New name for the GSLB service group.
+* `port` - (Optional) Server port number.
+* `publicip` - (Optional) The public IP address that a NAT device translates to the GSLB service's private IP address. Optional.
+* `publicport` - (Optional) The public port associated with the GSLB service's public IP address. The port is mapped to the service's private port number. Applicable to the local GSLB service. Optional.
+* `servername` - (Optional) Name of the server to which to bind the service group.
+* `servicetype` - (Optional) Protocol used to exchange data with the GSLB service.
+* `sitename` - (Optional) Name of the GSLB site to which the service group belongs.
+* `sitepersistence` - (Optional) Use cookie-based site persistence. Applicable only to HTTP and SSL non-autoscale enabled GSLB servicegroups.
+* `siteprefix` - (Optional) The site's prefix string. When the GSLB service group is bound to a GSLB virtual server, a GSLB site domain is generated internally for each bound serviceitem-domain pair by concatenating the site prefix of the service item and the name of the domain. If the special string NONE is specified, the site-prefix string is unset. When implementing HTTP redirect site persistence, the Citrix ADC redirects GSLB requests to GSLB services by using their site domains.
+* `state` - (Optional) Initial state of the GSLB service group.
+* `svrtimeout` - (Optional) Time, in seconds, after which to terminate an idle server connection.
+* `weight` - (Optional) Weight to assign to the servers in the service group. Specifies the capacity of the servers relative to the other servers in the load balancing configuration. The higher the weight, the higher the percentage of requests sent to the service.
+
+
+## Attribute Reference
+
+In addition to the arguments, the following attributes are available:
+
+* `id` - The id of the gslbservicegroup. It has the same value as the `servicegroupname` attribute.
+
+
+## Import
+
+A gslbservicegroup can be imported using its name, e.g.
+
+```shell
+terraform import citrixadc_gslbservicegroup.tf_gslbservicegroup test_gslbvservicegroup
+```
