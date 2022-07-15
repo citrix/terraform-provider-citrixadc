2757ec05f4df926d7781aea4fda929a5727f7c4f
christosm <christos.mersinis@citrix.com>
2022-07-15 15:10:26 +0000

    correct example files

--- examples/gslb/gslbparameter/resources.tf
+++ examples/gslb/gslbparameter/resources.tf
@@ -1,6 +1,6 @@
 resource "citrixadc_gslbparameter" "tf_gslbparameter" {
-  ldnsentrytimeout = 50.2
-  rtttolerance     = 6
+  ldnsentrytimeout = 50
+  rtttolerance     = 10
   ldnsmask         = "255.255.255.255"
 }
 
