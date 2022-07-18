5835c2f14b76ed99f9a0cb66077aead55eae667f
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2022-07-18 11:35:26 +0530

    Gslbparameter (#620)
    Co-authored-by: christosm <christos.mersinis@citrix.com>

---
+++ citrixadc/resource_citrixadc_gslbparameter.go
@@ -0,0 +1,257 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/gslb"
+
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+)
+
+func resourceCitrixAdcGslbparameter() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createGslbparameterFunc,
+		Read:          readGslbparameterFunc,
+		Update:        updateGslbparameterFunc,
+		Delete:        deleteGslbparameterFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"automaticconfigsync": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"dropldnsreq": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"gslbconfigsyncmonitor": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"gslbsvcstatedelaytime": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"gslbsyncinterval": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"gslbsynclocfiles": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"gslbsyncmode": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ldnsentrytimeout": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"ldnsmask": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ldnsprobeorder": &schema.Schema{
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"mepkeepalivetimeout": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"rtttolerance": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"svcstatelearningtime": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"v6ldnsmasklen": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+func createGslbparameterFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createGslbparameterFunc")
+	client := meta.(*NetScalerNitroClient).client
+	gslbparameterName := resource.PrefixedUniqueId("tf-gslbparameter-")
+
+	gslbparameter := gslb.Gslbparameter{
+		Automaticconfigsync:   d.Get("automaticconfigsync").(string),
+		Dropldnsreq:           d.Get("dropldnsreq").(string),
+		Gslbconfigsyncmonitor: d.Get("gslbconfigsyncmonitor").(string),
+		Gslbsvcstatedelaytime: d.Get("gslbsvcstatedelaytime").(int),
+		Gslbsyncinterval:      d.Get("gslbsyncinterval").(int),
+		Gslbsynclocfiles:      d.Get("gslbsynclocfiles").(string),
+		Gslbsyncmode:          d.Get("gslbsyncmode").(string),
+		Ldnsentrytimeout:      d.Get("ldnsentrytimeout").(int),
+		Ldnsmask:              d.Get("ldnsmask").(string),
+		Ldnsprobeorder:        toStringList(d.Get("ldnsprobeorder").([]interface{})),
+		Mepkeepalivetimeout:   d.Get("mepkeepalivetimeout").(int),
+		Rtttolerance:          d.Get("rtttolerance").(int),
+		Svcstatelearningtime:  d.Get("svcstatelearningtime").(int),
+		V6ldnsmasklen:         d.Get("v6ldnsmasklen").(int),
+	}
+
+	err := client.UpdateUnnamedResource(service.Gslbparameter.Type(), &gslbparameter)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(gslbparameterName)
+	err = readGslbparameterFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just updated this gslbparameter but we can't read it ?? ")
+		return nil
+	}
+	return nil
+}
+
+func readGslbparameterFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readGslbparameterFunc")
+	client := meta.(*NetScalerNitroClient).client
+	log.Printf("[DEBUG] citrixadc-provider: Reading gslbparameter state ")
+	data, err := client.FindResource(service.Gslbparameter.Type(), "") 
+	if err != nil {
+		log.Printf("[WARN] citrixadc-provider: Clearing gslbparameter state ")
+		d.SetId("")
+		return nil
+	}
+	d.Set("automaticconfigsync", data["automaticconfigsync"])
+	d.Set("dropldnsreq", data["dropldnsreq"])
+	d.Set("gslbconfigsyncmonitor", data["gslbconfigsyncmonitor"])
+	d.Set("gslbsvcstatedelaytime", data["gslbsvcstatedelaytime"])
+	d.Set("gslbsyncinterval", data["gslbsyncinterval"])
+	d.Set("gslbsynclocfiles", data["gslbsynclocfiles"])
+	d.Set("gslbsyncmode", data["gslbsyncmode"])
+	d.Set("ldnsentrytimeout", data["ldnsentrytimeout"])
+	d.Set("ldnsmask", data["ldnsmask"])
+	d.Set("ldnsprobeorder", data["ldnsprobeorder"])
+	d.Set("mepkeepalivetimeout", data["mepkeepalivetimeout"])
+	d.Set("rtttolerance", data["rtttolerance"])
+	d.Set("svcstatelearningtime", data["svcstatelearningtime"])
+	d.Set("v6ldnsmasklen", data["v6ldnsmasklen"])
+
+	return nil
+
+}
+
+func updateGslbparameterFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In updateGslbparameterFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+
+	gslbparameter := gslb.Gslbparameter{}
+	hasChange := false
+	if d.HasChange("automaticconfigsync") {
+		log.Printf("[DEBUG]  citrixadc-provider: Automaticconfigsync has changed for gslbparameter, starting update")
+		gslbparameter.Automaticconfigsync = d.Get("automaticconfigsync").(string)
+		hasChange = true
+	}
+	if d.HasChange("dropldnsreq") {
+		log.Printf("[DEBUG]  citrixadc-provider: Dropldnsreq has changed for gslbparameter, starting update")
+		gslbparameter.Dropldnsreq = d.Get("dropldnsreq").(string)
+		hasChange = true
+	}
+	if d.HasChange("gslbconfigsyncmonitor") {
+		log.Printf("[DEBUG]  citrixadc-provider: Gslbconfigsyncmonitor has changed for gslbparameter, starting update")
+		gslbparameter.Gslbconfigsyncmonitor = d.Get("gslbconfigsyncmonitor").(string)
+		hasChange = true
+	}
+	if d.HasChange("gslbsvcstatedelaytime") {
+		log.Printf("[DEBUG]  citrixadc-provider: Gslbsvcstatedelaytime has changed for gslbparameter, starting update")
+		gslbparameter.Gslbsvcstatedelaytime = d.Get("gslbsvcstatedelaytime").(int)
+		hasChange = true
+	}
+	if d.HasChange("gslbsyncinterval") {
+		log.Printf("[DEBUG]  citrixadc-provider: Gslbsyncinterval has changed for gslbparameter, starting update")
+		gslbparameter.Gslbsyncinterval = d.Get("gslbsyncinterval").(int)
+		hasChange = true
+	}
+	if d.HasChange("gslbsynclocfiles") {
+		log.Printf("[DEBUG]  citrixadc-provider: Gslbsynclocfiles has changed for gslbparameter, starting update")
+		gslbparameter.Gslbsynclocfiles = d.Get("gslbsynclocfiles").(string)
+		hasChange = true
+	}
+	if d.HasChange("gslbsyncmode") {
+		log.Printf("[DEBUG]  citrixadc-provider: Gslbsyncmode has changed for gslbparameter, starting update")
+		gslbparameter.Gslbsyncmode = d.Get("gslbsyncmode").(string)
+		hasChange = true
+	}
+	if d.HasChange("ldnsentrytimeout") {
+		log.Printf("[DEBUG]  citrixadc-provider: Ldnsentrytimeout has changed for gslbparameter, starting update")
+		gslbparameter.Ldnsentrytimeout = d.Get("ldnsentrytimeout").(int)
+		hasChange = true
+	}
+	if d.HasChange("ldnsmask") {
+		log.Printf("[DEBUG]  citrixadc-provider: Ldnsmask has changed for gslbparameter, starting update")
+		gslbparameter.Ldnsmask = d.Get("ldnsmask").(string)
+		hasChange = true
+	}
+	if d.HasChange("ldnsprobeorder") {
+		log.Printf("[DEBUG]  citrixadc-provider: Ldnsprobeorder has changed for gslbparameter, starting update")
+		gslbparameter.Ldnsprobeorder = toStringList(d.Get("ldnsprobeorder").([]interface{}))
+		hasChange = true
+	}
+	if d.HasChange("mepkeepalivetimeout") {
+		log.Printf("[DEBUG]  citrixadc-provider: Mepkeepalivetimeout has changed for gslbparameter, starting update")
+		gslbparameter.Mepkeepalivetimeout = d.Get("mepkeepalivetimeout").(int)
+		hasChange = true
+	}
+	if d.HasChange("rtttolerance") {
+		log.Printf("[DEBUG]  citrixadc-provider: Rtttolerance has changed for gslbparameter, starting update")
+		gslbparameter.Rtttolerance = d.Get("rtttolerance").(int)
+		hasChange = true
+	}
+	if d.HasChange("svcstatelearningtime") {
+		log.Printf("[DEBUG]  citrixadc-provider: Svcstatelearningtime has changed for gslbparameter, starting update")
+		gslbparameter.Svcstatelearningtime = d.Get("svcstatelearningtime").(int)
+		hasChange = true
+	}
+	if d.HasChange("v6ldnsmasklen") {
+		log.Printf("[DEBUG]  citrixadc-provider: V6ldnsmasklen has changed for gslbparameter, starting update")
+		gslbparameter.V6ldnsmasklen = d.Get("v6ldnsmasklen").(int)
+		hasChange = true
+	}
+
+	if hasChange {
+		err := client.UpdateUnnamedResource(service.Gslbparameter.Type(), &gslbparameter)
+		if err != nil {
+			return fmt.Errorf("Error updating gslbparameter: %s", err.Error())
+		}
+	}
+	return readGslbparameterFunc(d, meta)
+}
+
+func deleteGslbparameterFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteGslbparameterFunc")
+	// gslbparameter does not have DELETE operation, but this function is required to set the ID to ""
+	d.SetId("")
+
+	return nil
+}
---
+++ docs/resources/gslbparameter.md
@@ -0,0 +1,45 @@
+---
+subcategory: "GSLB"
+---
+
+# Resource: gslbparameter
+
+The gslbparameter resource is used to create gslbparameter.
+
+
+## Example usage
+
+```hcl
+resource "citrixadc_gslbparameter" "tf_gslbparameter" {
+  ldnsentrytimeout = 50
+  rtttolerance     = 10
+  ldnsmask         = "255.255.255.255"
+}
+
+```
+
+
+## Argument Reference
+
+* `automaticconfigsync` - (Optional) GSLB configuration will be synced automatically to remote gslb sites if enabled.
+* `dropldnsreq` - (Optional) Drop LDNS requests if round-trip time (RTT) information is not available.
+* `gslbconfigsyncmonitor` - (Optional) If enabled, remote gslb site's rsync port will be monitored and site is considered for configuration sync only when the monitor is successful.
+* `gslbsvcstatedelaytime` - (Optional) Amount of delay in updating the state of GSLB service to DOWN when MEP goes down. 			This parameter is applicable only if monitors are not bound to GSLB services
+* `gslbsyncinterval` - (Optional) Time duartion (in seconds) for which the gslb sync process will wait before checking for config changes.
+* `gslbsynclocfiles` - (Optional) If disabled, Location files will not be synced to the remote sites as part of automatic sync.
+* `gslbsyncmode` - (Optional) Mode in which configuration will be synced from master site to remote sites.
+* `ldnsentrytimeout` - (Optional) Time, in seconds, after which an inactive LDNS entry is removed.
+* `ldnsmask` - (Optional) The IPv4 network mask with which to create LDNS entries.
+* `ldnsprobeorder` - (Optional) Order in which monitors should be initiated to calculate RTT.
+* `mepkeepalivetimeout` - (Optional) Time duartion (in seconds) during which if no new packets received by Local gslb site from Remote gslb site then mark the MEP connection DOWN
+* `rtttolerance` - (Optional) Tolerance, in milliseconds, for newly learned round-trip time (RTT) values. If the difference between the old RTT value and the newly computed RTT value is less than or equal to the specified tolerance value, the LDNS entry in the network metric table is not updated with the new RTT value. Prevents the exchange of metrics when variations in RTT values are negligible.
+* `svcstatelearningtime` - (Optional) Time (in seconds) within which local or child site services remain in learning phase. GSLB site will enter the learning phase after reboot, HA failover, Cluster GSLB owner node changes or MEP being enabled on local node.  Backup parent (if configured) will selectively move the adopted children's GSLB services to learning phase when primary parent goes down. While a service is in learning period, remote site will not honour the state and stats got through MEP for that service. State can be learnt from health monitor if bound explicitly.
+* `v6ldnsmasklen` - (Optional) Mask for creating LDNS entries for IPv6 source addresses. The mask is defined as the number of leading bits to consider, in the source IP address, when creating an LDNS entry.
+
+
+## Attribute Reference
+
+In addition to the arguments, the following attributes are available:
+
+* `id` - The id of the gslbparameter. It is a unique string prefixed with "tf-gslbparameter-"
+
