5f68df04933db10123f94592679bdb965563cd93
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2022-07-21 12:59:48 +0530

    Crvserver appfwpolicy binding (#640)
    Co-authored-by: Shivashankar Vaddepally <shivashankar.vaddepally@citrix.com>

---
+++ examples/cacheredirection/crvserver_appfwpolicy_binding/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+    required_providers {
+        citrixadc = {
+            source = "citrix/citrixadc"
+        }
+    }
+}
+
+provider "citrixadc" {
+  endpoint = "http://10.222.74.157"
+  username = "nsroot"
+  password = "notnsroot"
+}
