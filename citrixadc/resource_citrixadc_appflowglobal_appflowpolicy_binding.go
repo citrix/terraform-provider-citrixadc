582b004328679ec2d35468c114ff8cce897ea151
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2022-08-04 07:17:35 +0530

    **New Resource** appflowglobal_appflowpolicy_binding (#675)
    Co-authored-by: christosm <christos.mersinis@citrix.com>

---
+++ citrixadc/resource_citrixadc_appflowglobal_appflowpolicy_binding.go
@@ -0,0 +1,192 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/appflow"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+	"net/url"
+)
+
+func resourceCitrixAdcAppflowglobal_appflowpolicy_binding() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createAppflowglobal_appflowpolicy_bindingFunc,
+		Read:          readAppflowglobal_appflowpolicy_bindingFunc,
+		Delete:        deleteAppflowglobal_appflowpolicy_bindingFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"globalbindtype": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"policyname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"gotopriorityexpression": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"invoke": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"labelname": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"labeltype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"priority": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func createAppflowglobal_appflowpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createAppflowglobal_appflowpolicy_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	policyname := d.Get("policyname").(string)
+	
+	
+	appflowglobal_appflowpolicy_binding := appflow.Appflowglobalappflowpolicybinding{
+		Globalbindtype:         d.Get("globalbindtype").(string),
+		Gotopriorityexpression: d.Get("gotopriorityexpression").(string),
+		Invoke:                 d.Get("invoke").(bool),
+		Labelname:              d.Get("labelname").(string),
+		Labeltype:              d.Get("labeltype").(string),
+		Policyname:             d.Get("policyname").(string),
+		Priority:               d.Get("priority").(int),
+		Type:                   d.Get("type").(string),
+	}
+
+	err := client.UpdateUnnamedResource(service.Appflowglobal_appflowpolicy_binding.Type(), &appflowglobal_appflowpolicy_binding)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(policyname)
+
+	err = readAppflowglobal_appflowpolicy_bindingFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this appflowglobal_appflowpolicy_binding but we can't read it ?? %s", policyname)
+		return nil
+	}
+	return nil
+}
+
+func readAppflowglobal_appflowpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readAppflowglobal_appflowpolicy_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	policyname:= d.Id()
+
+	log.Printf("[DEBUG] citrixadc-provider: Reading appflowglobal_appflowpolicy_binding state %s", policyname)
+
+	findParams := service.FindParams{
+		ResourceType:             "appflowglobal_appflowpolicy_binding",
+		ArgsMap: 				  map[string]string{ "type":d.Get("type").(string) },
+		ResourceMissingErrorCode: 258,
+	}
+	dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+	// Unexpected error
+	if err != nil {
+		log.Printf("[DEBUG] citrixadc-provider: Error during FindResourceArrayWithParams %s", err.Error())
+		return err
+	}
+
+	// Resource is missing
+	if len(dataArr) == 0 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams returned empty array")
+		log.Printf("[WARN] citrixadc-provider: Clearing appflowglobal_appflowpolicy_binding state %s", policyname)
+		d.SetId("")
+		return nil
+	}
+
+	// Iterate through results to find the one with the right id
+	foundIndex := -1
+	for i, v := range dataArr {
+		if v["policyname"].(string) == policyname {
+			foundIndex = i
+			break
+		}
+	}
+
+	// Resource is missing
+	if foundIndex == -1 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams policyname not found in array")
+		log.Printf("[WARN] citrixadc-provider: Clearing appflowglobal_appflowpolicy_binding state %s", policyname)
+		d.SetId("")
+		return nil
+	}
+	// Fallthrough
+
+	data := dataArr[foundIndex]
+
+	d.Set("globalbindtype", data["globalbindtype"])
+	d.Set("gotopriorityexpression", data["gotopriorityexpression"])
+	d.Set("invoke", data["invoke"])
+	d.Set("labelname", data["labelname"])
+	d.Set("labeltype", data["labeltype"])
+	d.Set("policyname", data["policyname"])
+	d.Set("priority", data["priority"])
+	d.Set("type", data["type"])
+
+	return nil
+
+}
+
+func deleteAppflowglobal_appflowpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteAppflowglobal_appflowpolicy_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	policyname := d.Id()
+
+	args := make([]string, 0)
+	args = append(args, fmt.Sprintf("policyname:%s", policyname))
+	
+	if v, ok := d.GetOk("type"); ok {
+		typeName := v.(string)
+		args = append(args, fmt.Sprintf("type:%s", url.QueryEscape(typeName)))
+	}
+	
+	if v, ok := d.GetOk("priority"); ok {
+		priority := v.(int)
+		args = append(args, fmt.Sprintf("priority:%d", priority))
+	}
+
+	err := client.DeleteResourceWithArgs(service.Appflowglobal_appflowpolicy_binding.Type(), "", args)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
