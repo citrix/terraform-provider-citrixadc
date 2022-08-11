beea1bad1ab726a3d67b041854151b3291f0b758
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2022-08-11 11:41:49 +0530

    **New Resource** responderglobal_responderpolicy_binding (#704)
    Co-authored-by: christosm <christos.mersinis@citrix.com>

---
+++ citrixadc/resource_citrixadc_responderglobal_responderpolicy_binding.go
@@ -0,0 +1,195 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/responder"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"net/url"
+	"log"
+)
+
+func resourceCitrixAdcResponderglobal_responderpolicy_binding() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createResponderglobal_responderpolicy_bindingFunc,
+		Read:          readResponderglobal_responderpolicy_bindingFunc,
+		Delete:        deleteResponderglobal_responderpolicy_bindingFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"policyname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"priority": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+				ForceNew: true,
+			},
+			"globalbindtype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"gotopriorityexpression": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"invoke": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"labelname": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"labeltype": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func createResponderglobal_responderpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createResponderglobal_responderpolicy_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	policyname := d.Get("policyname").(string)
+	responderglobal_responderpolicy_binding := responder.Responderglobalresponderpolicybinding{
+		Globalbindtype:         d.Get("globalbindtype").(string),
+		Gotopriorityexpression: d.Get("gotopriorityexpression").(string),
+		Invoke:                 d.Get("invoke").(bool),
+		Labelname:              d.Get("labelname").(string),
+		Labeltype:              d.Get("labeltype").(string),
+		Policyname:             d.Get("policyname").(string),
+		Priority:               d.Get("priority").(int),
+		Type:                   d.Get("type").(string),
+	}
+
+	err := client.UpdateUnnamedResource(service.Responderglobal_responderpolicy_binding.Type(), &responderglobal_responderpolicy_binding)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(policyname)
+
+	err = readResponderglobal_responderpolicy_bindingFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this responderglobal_responderpolicy_binding but we can't read it ?? %s", policyname)
+		return nil
+	}
+	return nil
+}
+
+func readResponderglobal_responderpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readResponderglobal_responderpolicy_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	policyname := d.Id()
+
+	log.Printf("[DEBUG] citrixadc-provider: Reading responderglobal_responderpolicy_binding state %s", policyname)
+
+	argsMap := make(map[string]string)
+	if v, ok := d.GetOk("type"); ok {
+		argsMap["type"] = url.QueryEscape(v.(string))
+	//if type is not set by user, we set it with the default value, "RES_DEFAULT"
+	} else {
+		argsMap["type"] = url.QueryEscape("REQ_DEFAULT")
+	}
+
+	findParams := service.FindParams{
+		ResourceType:             "responderglobal_responderpolicy_binding",
+		ArgsMap:                  argsMap,
+		ResourceMissingErrorCode: 258,
+	}
+	dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+	// Unexpected error
+	if err != nil {
+		log.Printf("[DEBUG] citrixadc-provider: Error during FindResourceArrayWithParams %s", err.Error())
+		return err
+	}
+
+	// Resource is missing
+	if len(dataArr) == 0 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams returned empty array")
+		log.Printf("[WARN] citrixadc-provider: Clearing responderglobal_responderpolicy_binding state %s", policyname)
+		d.SetId("")
+		return nil
+	}
+
+	// Iterate through results to find the one with the right id
+	foundIndex := -1
+	for i, v := range dataArr {
+		if v["policyname"].(string) == policyname {
+			foundIndex = i
+			break
+		}
+	}
+
+	// Resource is missing
+	if foundIndex == -1 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams secondIdComponent not found in array")
+		log.Printf("[WARN] citrixadc-provider: Clearing responderglobal_responderpolicy_binding state %s", policyname)
+		d.SetId("")
+		return nil
+	}
+	// Fallthrough
+
+	data := dataArr[foundIndex]
+
+	d.Set("globalbindtype", data["globalbindtype"])
+	d.Set("gotopriorityexpression", data["gotopriorityexpression"])
+	d.Set("invoke", data["invoke"])
+	d.Set("labelname", data["labelname"])
+	d.Set("labeltype", data["labeltype"])
+	d.Set("policyname", data["policyname"])
+	d.Set("priority", data["priority"])
+	d.Set("type", data["type"])
+
+	return nil
+
+}
+
+func deleteResponderglobal_responderpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteResponderglobal_responderpolicy_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+	policyname:= d.Id()
+	args := make([]string, 0)
+	args = append(args, fmt.Sprintf("policyname:%s", url.QueryEscape(policyname)))
+
+	if v, ok := d.GetOk("type"); ok {
+		args = append(args, fmt.Sprintf("type:%s", url.QueryEscape(v.(string))))
+	}
+	if v, ok := d.GetOk("priority"); ok {
+		args = append(args, fmt.Sprintf("priority:%v", v.(int)))
+	}
+
+	err := client.DeleteResourceWithArgs(service.Responderglobal_responderpolicy_binding.Type(), "", args)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
