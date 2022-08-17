99663fd526fb9ea786f1b0895bcb1116219376c8
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2022-08-17 23:03:22 +0530

    **New Resource** ipv6 (#722)
    Signed-off-by: christosm <christos.mersinis@citrix.com>
    Co-authored-by: christosm <christos.mersinis@citrix.com>

---
+++ citrixadc/resource_citrixadc_ipv6.go
@@ -0,0 +1,200 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"strconv"
+	"log"
+)
+
+func resourceCitrixAdcIpv6() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createIpv6Func,
+		Read:          readIpv6Func,
+		Update:        updateIpv6Func,
+		Delete:        deleteIpv6Func,
+		Schema: map[string]*schema.Schema{
+			"dodad": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"natprefix": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"ndbasereachtime": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"ndretransmissiontime": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"ralearning": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"routerredirection": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"td": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				Computed: true,
+			},
+			"usipnatprefix": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func createIpv6Func(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createIpv6Func")
+	client := meta.(*NetScalerNitroClient).client
+	ipv6Name := resource.PrefixedUniqueId("tf-ipv6-")
+	ipv6 := make(map[string]interface{})
+
+	if v, ok := d.GetOk("dodad"); ok {
+		ipv6["dodad"] = v.(string)	
+	}
+	if v, ok := d.GetOk("natprefix"); ok {
+		ipv6["natprefix"] = v.(string)	
+	}
+	if v, ok := d.GetOk("ndbasereachtime"); ok {
+		ipv6["ndbasereachtime"] = v.(int)	
+	}
+	if v, ok := d.GetOk("ndretransmissiontime"); ok {
+		ipv6["ndretransmissiontime"] = v.(int)	
+	}
+	if v, ok := d.GetOk("ralearning"); ok {
+		ipv6["ralearning"] = v.(string)
+		
+	}
+	if v, ok := d.GetOk("routerredirection"); ok {
+		ipv6["routerredirection"] = v.(string)	
+	}
+	if v, ok := d.GetOk("td"); ok {
+		ipv6["td"] = v.(int)	
+	}
+	if v, ok := d.GetOk("usipnatprefix"); ok {
+		ipv6["usipnatprefix"] = v.(string)
+	}
+
+
+	err := client.UpdateUnnamedResource(service.Ipv6.Type(), &ipv6)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(ipv6Name)
+
+	err = readIpv6Func(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this ipv6 but we can't read it ??")
+		return nil
+	}
+	return nil
+}
+
+func readIpv6Func(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readIpv6Func")
+	client := meta.(*NetScalerNitroClient).client
+	log.Printf("[DEBUG] citrixadc-provider: Reading ipv6 state")
+	data, err := client.FindResource(service.Ipv6.Type(), strconv.Itoa(d.Get("td").(int)))
+	if err != nil {
+		log.Printf("[WARN] citrixadc-provider: Clearing ipv6 state")
+		d.SetId("")
+		return nil
+	}
+	d.Set("td", data["td"])
+	d.Set("dodad", data["dodad"])
+	d.Set("natprefix", data["natprefix"])
+	d.Set("ndbasereachtime", data["ndbasereachtime"])
+	d.Set("ndretransmissiontime", data["ndretransmissiontime"])
+	d.Set("ralearning", data["ralearning"])
+	d.Set("routerredirection", data["routerredirection"])
+	d.Set("td", data["td"])
+	d.Set("usipnatprefix", data["usipnatprefix"])
+
+	return nil
+
+}
+
+func updateIpv6Func(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In updateIpv6Func")
+	client := meta.(*NetScalerNitroClient).client
+
+	ipv6 := make(map[string]interface{})
+
+	hasChange := false
+	if d.HasChange("dodad") {
+		log.Printf("[DEBUG]  citrixadc-provider: Dodad has changed for ipv6, starting update")
+		ipv6["dodad"] = d.Get("dodad").(string)
+		hasChange = true
+	}
+	if d.HasChange("natprefix") {
+		log.Printf("[DEBUG]  citrixadc-provider: Natprefix has changed for ipv6, starting update")
+		ipv6["natprefix"] = d.Get("natprefix").(string)
+		hasChange = true
+	}
+	if d.HasChange("ndbasereachtime") {
+		log.Printf("[DEBUG]  citrixadc-provider: Ndbasereachtime has changed for ipv6, starting update")
+		ipv6["ndbasereachtime"] = d.Get("ndbasereachtime").(int)
+		hasChange = true
+	}
+	if d.HasChange("ndretransmissiontime") {
+		log.Printf("[DEBUG]  citrixadc-provider: Ndretransmissiontime has changed for ipv6, starting update")
+		ipv6["ndretransmissiontime"] = d.Get("ndretransmissiontime").(int)
+		hasChange = true
+	}
+	if d.HasChange("ralearning") {
+		log.Printf("[DEBUG]  citrixadc-provider: Ralearning has changed for ipv6, starting update")
+		ipv6["ralearning"] = d.Get("ralearning").(string)
+		hasChange = true
+	}
+	if d.HasChange("routerredirection") {
+		log.Printf("[DEBUG]  citrixadc-provider: Routerredirection has changed for ipv6, starting update")
+		ipv6["routerredirection"] = d.Get("routerredirection").(string)
+		hasChange = true
+	}
+	if d.HasChange("td") {
+		log.Printf("[DEBUG]  citrixadc-provider: Td has changed for ipv6, starting update")
+		ipv6["td"] = d.Get("td").(int)
+		hasChange = true
+	}
+	if d.HasChange("usipnatprefix") {
+		log.Printf("[DEBUG]  citrixadc-provider: Usipnatprefix has changed for ipv6, starting update")
+		ipv6["usipnatprefix"] = d.Get("usipnatprefix").(string)
+		hasChange = true
+	}
+
+	if hasChange {
+		err := client.UpdateUnnamedResource(service.Ipv6.Type(), &ipv6)
+		if err != nil {
+			return fmt.Errorf("Error updating ipv6")
+		}
+	}
+	return readIpv6Func(d, meta)
+}
+
+func deleteIpv6Func(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteIpv6Func")
+	// ipv6 does not support DELETE operation
+	d.SetId("")
+
+	return nil
+}
---
+++ citrixadc/resource_citrixadc_ipv6_test.go
@@ -0,0 +1,126 @@
+/*
+Copyright 2016 Citrix Systems, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package citrixadc
+
+import (
+	"fmt"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"testing"
+)
+
+const testAccIpv6_basic = `
+
+resource "citrixadc_ipv6" "tf_ipv6" {
+	ralearning        = "DISABLED"
+	ndbasereachtime   = 4000
+	routerredirection = "DISABLED"
+  }
+  
+`
+const testAccIpv6_update = `
+
+resource "citrixadc_ipv6" "tf_ipv6" {
+	ralearning        = "ENABLED"
+	ndbasereachtime   = 4000
+	routerredirection = "ENABLED"
+  }
+`
+
+func TestAccIpv6_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckIpv6Destroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccIpv6_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIpv6Exist("citrixadc_ipv6.tf_ipv6", nil),
+					resource.TestCheckResourceAttr("citrixadc_ipv6.tf_ipv6", "ralearning", "DISABLED"),
+					resource.TestCheckResourceAttr("citrixadc_ipv6.tf_ipv6", "ndbasereachtime", "4000"),
+					resource.TestCheckResourceAttr("citrixadc_ipv6.tf_ipv6", "routerredirection", "DISABLED"),		
+				),
+			},
+			resource.TestStep{
+				Config: testAccIpv6_update,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckIpv6Exist("citrixadc_ipv6.tf_ipv6", nil),
+					resource.TestCheckResourceAttr("citrixadc_ipv6.tf_ipv6", "ralearning", "ENABLED"),
+					resource.TestCheckResourceAttr("citrixadc_ipv6.tf_ipv6", "ndbasereachtime", "4000"),
+					resource.TestCheckResourceAttr("citrixadc_ipv6.tf_ipv6", "routerredirection", "ENABLED"),		
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckIpv6Exist(n string, id *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ipv6 name is set")
+		}
+
+		if id != nil {
+			if *id != "" && *id != rs.Primary.ID {
+				return fmt.Errorf("Resource ID has changed!")
+			}
+
+			*id = rs.Primary.ID
+		}
+
+		nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+		data, err := nsClient.FindResource(service.Ipv6.Type(), rs.Primary.Attributes["td"])
+
+		if err != nil {
+			return err
+		}
+
+		if data == nil {
+			return fmt.Errorf("ipv6 %s not found", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckIpv6Destroy(s *terraform.State) error {
+	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "citrixadc_ipv6" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No name is set")
+		}
+
+		_, err := nsClient.FindResource(service.Ipv6.Type(), rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("ipv6 %s still exists", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
