c71d2ce986d5102dc70f089e31d40b56fc9efb5d
christosm <christos.mersinis@citrix.com>
2022-08-17 11:50:34 +0000

    **New Resource** aaauser_vpntrafficpolicy_binding
    Signed-off-by: christosm <christos.mersinis@citrix.com>

---
+++ citrixadc/resource_citrixadc_aaauser_vpntrafficpolicy_binding.go
@@ -0,0 +1,170 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/aaa"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+	"strings"
+)
+
+func resourceCitrixAdcAaauser_vpntrafficpolicy_binding() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createAaauser_vpntrafficpolicy_bindingFunc,
+		Read:          readAaauser_vpntrafficpolicy_bindingFunc,
+		Delete:        deleteAaauser_vpntrafficpolicy_bindingFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"username": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"policy": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"priority": &schema.Schema{
+				Type:     schema.TypeInt,
+				Required: true,
+				ForceNew: true,
+			},
+			"gotopriorityexpression": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"type": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func createAaauser_vpntrafficpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createAaauser_vpntrafficpolicy_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	username := d.Get("username").(string)
+	policy := d.Get("policy").(string)
+	bindingId := fmt.Sprintf("%s,%s", username, policy)
+	aaauser_vpntrafficpolicy_binding := aaa.Aaauservpntrafficpolicybinding{
+		Gotopriorityexpression: d.Get("gotopriorityexpression").(string),
+		Policy:                 d.Get("policy").(string),
+		Priority:               d.Get("priority").(int),
+		Type:                   d.Get("type").(string),
+		Username:               d.Get("username").(string),
+	}
+
+	err := client.UpdateUnnamedResource(service.Aaauser_vpntrafficpolicy_binding.Type(), &aaauser_vpntrafficpolicy_binding)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(bindingId)
+
+	err = readAaauser_vpntrafficpolicy_bindingFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this aaauser_vpntrafficpolicy_binding but we can't read it ?? %s", bindingId)
+		return nil
+	}
+	return nil
+}
+
+func readAaauser_vpntrafficpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readAaauser_vpntrafficpolicy_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	username := idSlice[0]
+	policy := idSlice[1]
+
+	log.Printf("[DEBUG] citrixadc-provider: Reading aaauser_vpntrafficpolicy_binding state %s", bindingId)
+
+	findParams := service.FindParams{
+		ResourceType:             "aaauser_vpntrafficpolicy_binding",
+		ResourceName:             username,
+		ResourceMissingErrorCode: 258,
+	}
+	dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+	// Unexpected error
+	if err != nil {
+		log.Printf("[DEBUG] citrixadc-provider: Error during FindResourceArrayWithParams %s", err.Error())
+		return err
+	}
+
+	// Resource is missing
+	if len(dataArr) == 0 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams returned empty array")
+		log.Printf("[WARN] citrixadc-provider: Clearing aaauser_vpntrafficpolicy_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+
+	// Iterate through results to find the one with the right id
+	foundIndex := -1
+	for i, v := range dataArr {
+		if v["policy"].(string) == policy {
+			foundIndex = i
+			break
+		}
+	}
+
+	// Resource is missing
+	if foundIndex == -1 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams policy not found in array")
+		log.Printf("[WARN] citrixadc-provider: Clearing aaauser_vpntrafficpolicy_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+	// Fallthrough
+
+	data := dataArr[foundIndex]
+
+	d.Set("gotopriorityexpression", data["gotopriorityexpression"])
+	d.Set("policy", data["policy"])
+	d.Set("priority", data["priority"])
+	d.Set("type", data["type"])
+	d.Set("username", data["username"])
+
+	return nil
+
+}
+
+func deleteAaauser_vpntrafficpolicy_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteAaauser_vpntrafficpolicy_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	name := idSlice[0]
+	policy := idSlice[1]
+
+	args := make([]string, 0)
+	args = append(args, fmt.Sprintf("policy:%s", policy))
+	if v, ok := d.GetOk("type"); ok {
+		type_val := v.(string)
+		args = append(args, fmt.Sprintf("type:%s", type_val))
+	}
+
+	err := client.DeleteResourceWithArgs(service.Aaauser_vpntrafficpolicy_binding.Type(), name, args)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
