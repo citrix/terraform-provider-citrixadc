10190c113f17a733df5f495f4b75dd83ff21b2fe
Sumanth Lingappa <42572246+sumanth-lingappa@users.noreply.github.com>
2022-08-18 22:40:53 +0530

    **New Resource** aaagroup (#742)
    Signed-off-by: christosm <christos.mersinis@citrix.com>
    
    Signed-off-by: christosm <christos.mersinis@citrix.com>
    Co-authored-by: christosm <christos.mersinis@citrix.com>

---
+++ citrixadc/resource_citrixadc_aaagroup.go
@@ -0,0 +1,94 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/aaa"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"log"
+)
+
+func resourceCitrixAdcAaagroup() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createAaagroupFunc,
+		Read:          readAaagroupFunc,
+		Delete:        deleteAaagroupFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"groupname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"loggedin": &schema.Schema{
+				Type:     schema.TypeBool,
+				Optional: true,
+				ForceNew: true,
+			},
+			"weight": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func createAaagroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createAaagroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	aaagroupName := d.Get("groupname").(string)
+	aaagroup := aaa.Aaagroup{
+		Groupname: d.Get("groupname").(string),
+		Weight:    d.Get("weight").(int),
+	}
+
+	_, err := client.AddResource(service.Aaagroup.Type(), aaagroupName, &aaagroup)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(aaagroupName)
+
+	err = readAaagroupFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this aaagroup but we can't read it ?? %s", aaagroupName)
+		return nil
+	}
+	return nil
+}
+
+func readAaagroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readAaagroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	aaagroupName := d.Id()
+	log.Printf("[DEBUG] citrixadc-provider: Reading aaagroup state %s", aaagroupName)
+	data, err := client.FindResource(service.Aaagroup.Type(), aaagroupName)
+	if err != nil {
+		log.Printf("[WARN] citrixadc-provider: Clearing aaagroup state %s", aaagroupName)
+		d.SetId("")
+		return nil
+	}
+	d.Set("groupname", data["groupname"])
+	d.Set("loggedin", data["loggedin"])
+	d.Set("weight", data["weight"])
+
+	return nil
+
+}
+func deleteAaagroupFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteAaagroupFunc")
+	client := meta.(*NetScalerNitroClient).client
+	aaagroupName := d.Id()
+	err := client.DeleteResource(service.Aaagroup.Type(), aaagroupName)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
---
+++ examples/aaa/aaagroup/resources.tf
@@ -0,0 +1,4 @@
+resource "citrixadc_aaagroup" "tf_aaagroup" {
+  groupname = "my_group"
+  weight    = 50
+}
