dde3661cb77c249ef2dc67d8502a90e3506930ee
christosm <christos.mersinis@citrix.com>
2022-09-15 06:44:21 +0000

    **New Resource** lsngroup_lsnpool_binding
    Signed-off-by: christosm <christos.mersinis@citrix.com>

---
+++ citrixadc/resource_citrixadc_lsngroup_lsnpool_binding.go
@@ -0,0 +1,143 @@
+package citrixadc
+
+import (
+	"github.com/citrix/adc-nitro-go/resource/config/lsn"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/schema"
+
+	"fmt"
+	"log"
+	"strings"
+)
+
+func resourceCitrixAdcLsngroup_lsnpool_binding() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 1,
+		Create:        createLsngroup_lsnpool_bindingFunc,
+		Read:          readLsngroup_lsnpool_bindingFunc,
+		Delete:        deleteLsngroup_lsnpool_bindingFunc,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+		Schema: map[string]*schema.Schema{
+			"groupname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"poolname": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func createLsngroup_lsnpool_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In createLsngroup_lsnpool_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	groupname := d.Get("groupname")
+	poolname := d.Get("poolname")
+	bindingId := fmt.Sprintf("%s,%s", groupname, poolname)
+	lsngroup_lsnpool_binding := lsn.Lsngrouplsnpoolbinding{
+		Groupname: d.Get("groupname").(string),
+		Poolname:  d.Get("poolname").(string),
+	}
+
+	err := client.UpdateUnnamedResource("lsngroup_lsnpool_binding", &lsngroup_lsnpool_binding)
+	if err != nil {
+		return err
+	}
+
+	d.SetId(bindingId)
+
+	err = readLsngroup_lsnpool_bindingFunc(d, meta)
+	if err != nil {
+		log.Printf("[ERROR] netscaler-provider: ?? we just created this lsngroup_lsnpool_binding but we can't read it ?? %s", bindingId)
+		return nil
+	}
+	return nil
+}
+
+func readLsngroup_lsnpool_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG] citrixadc-provider:  In readLsngroup_lsnpool_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	groupname := idSlice[0]
+	poolname := idSlice[1]
+
+	log.Printf("[DEBUG] citrixadc-provider: Reading lsngroup_lsnpool_binding state %s", bindingId)
+
+	findParams := service.FindParams{
+		ResourceType:             "lsngroup_lsnpool_binding",
+		ResourceName:             groupname,
+		ResourceMissingErrorCode: 258,
+	}
+	dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+	// Unexpected error
+	if err != nil {
+		log.Printf("[DEBUG] citrixadc-provider: Error during FindResourceArrayWithParams %s", err.Error())
+		return err
+	}
+
+	// Resource is missing
+	if len(dataArr) == 0 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams returned empty array")
+		log.Printf("[WARN] citrixadc-provider: Clearing lsngroup_lsnpool_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+
+	// Iterate through results to find the one with the right id
+	foundIndex := -1
+	for i, v := range dataArr {
+		if v["poolname"].(string) == poolname {
+			foundIndex = i
+			break
+		}
+	}
+
+	// Resource is missing
+	if foundIndex == -1 {
+		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams poolname not found in array")
+		log.Printf("[WARN] citrixadc-provider: Clearing lsngroup_lsnpool_binding state %s", bindingId)
+		d.SetId("")
+		return nil
+	}
+	// Fallthrough
+
+	data := dataArr[foundIndex]
+
+	d.Set("groupname", data["groupname"])
+	d.Set("poolname", data["poolname"])
+
+	return nil
+
+}
+
+func deleteLsngroup_lsnpool_bindingFunc(d *schema.ResourceData, meta interface{}) error {
+	log.Printf("[DEBUG]  citrixadc-provider: In deleteLsngroup_lsnpool_bindingFunc")
+	client := meta.(*NetScalerNitroClient).client
+
+	bindingId := d.Id()
+	idSlice := strings.SplitN(bindingId, ",", 2)
+
+	name := idSlice[0]
+	poolname := idSlice[1]
+
+	args := make([]string, 0)
+	args = append(args, fmt.Sprintf("poolname:%s", poolname))
+
+	err := client.DeleteResourceWithArgs("lsngroup_lsnpool_binding", name, args)
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
+	return nil
+}
---
+++ citrixadc/resource_citrixadc_lsngroup_lsnpool_binding_test.go
@@ -0,0 +1,180 @@
+/*
+Copyright 2016 Citrix Systems, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package citrixadc
+
+import (
+	"fmt"
+	"github.com/citrix/adc-nitro-go/service"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"strings"
+	"testing"
+)
+
+const testAccLsngroup_lsnpool_binding_basic = `
+
+	resource "citrixadc_lsngroup_lsnpool_binding" "tf_lsngroup_lsnpool_binding" {
+		groupname = "my_lsn_group"
+		poolname  = "my_pool"
+	}
+  
+`
+
+const testAccLsngroup_lsnpool_binding_basic_step2 = `
+	# Keep the above bound resources without the actual binding to check proper deletion
+`
+
+func TestAccLsngroup_lsnpool_binding_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLsngroup_lsnpool_bindingDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccLsngroup_lsnpool_binding_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLsngroup_lsnpool_bindingExist("citrixadc_lsngroup_lsnpool_binding.tf_lsngroup_lsnpool_binding", nil),
+				),
+			},
+			resource.TestStep{
+				Config: testAccLsngroup_lsnpool_binding_basic_step2,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLsngroup_lsnpool_bindingNotExist("citrixadc_lsngroup_lsnpool_binding.tf_lsngroup_lsnpool_binding", "my_lsn_group,my_pool"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckLsngroup_lsnpool_bindingExist(n string, id *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No lsngroup_lsnpool_binding id is set")
+		}
+
+		if id != nil {
+			if *id != "" && *id != rs.Primary.ID {
+				return fmt.Errorf("Resource ID has changed!")
+			}
+
+			*id = rs.Primary.ID
+		}
+
+		client := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+		bindingId := rs.Primary.ID
+
+		idSlice := strings.SplitN(bindingId, ",", 2)
+
+		groupname := idSlice[0]
+		poolname := idSlice[1]
+
+		findParams := service.FindParams{
+			ResourceType:             "lsngroup_lsnpool_binding",
+			ResourceName:             groupname,
+			ResourceMissingErrorCode: 258,
+		}
+		dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+		// Unexpected error
+		if err != nil {
+			return err
+		}
+
+		// Iterate through results to find the one with the matching poolname
+		found := false
+		for _, v := range dataArr {
+			if v["poolname"].(string) == poolname {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return fmt.Errorf("lsngroup_lsnpool_binding %s not found", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckLsngroup_lsnpool_bindingNotExist(n string, id string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		client := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+		if !strings.Contains(id, ",") {
+			return fmt.Errorf("Invalid id string %v. The id string must contain a comma.", id)
+		}
+		idSlice := strings.SplitN(id, ",", 2)
+
+		groupname := idSlice[0]
+		poolname := idSlice[1]
+
+		findParams := service.FindParams{
+			ResourceType:             "lsngroup_lsnpool_binding",
+			ResourceName:             groupname,
+			ResourceMissingErrorCode: 258,
+		}
+		dataArr, err := client.FindResourceArrayWithParams(findParams)
+
+		// Unexpected error
+		if err != nil {
+			return err
+		}
+
+		// Iterate through results to hopefully not find the one with the matching poolname
+		found := false
+		for _, v := range dataArr {
+			if v["poolname"].(string) == poolname {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			return fmt.Errorf("lsngroup_lsnpool_binding %s was found, but it should have been destroyed", n)
+		}
+
+		return nil
+	}
+}
+
+func testAccCheckLsngroup_lsnpool_bindingDestroy(s *terraform.State) error {
+	nsClient := testAccProvider.Meta().(*NetScalerNitroClient).client
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "citrixadc_lsngroup_lsnpool_binding" {
+			continue
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No name is set")
+		}
+
+		_, err := nsClient.FindResource("lsngroup_lsnpool_binding", rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("lsngroup_lsnpool_binding %s still exists", rs.Primary.ID)
+		}
+
+	}
+
+	return nil
+}
