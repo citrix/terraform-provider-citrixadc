6b384f4d57a73d2db89dec3b9b2a02485386b8c8
Sumanth Lingappa <sumanth.lingappa@citrix.com>
2022-09-28 04:04:20 +0000

    deleted aaa example folder
    Signed-off-by: Sumanth Lingappa <sumanth.lingappa@citrix.com>

--- examples/aaa/aaaglobal_aaapreauthenticationpolicy_binding/resources.tf
+++
@@ -1,4 +0,0 @@
-resource "citrixadc_aaaglobal_aaapreauthenticationpolicy_binding" "tf_aaaglobal_aaapreauthenticationpolicy_binding" {
-  policy    = "my_preauthentication_policy"
-  priority  = 50
-  }
