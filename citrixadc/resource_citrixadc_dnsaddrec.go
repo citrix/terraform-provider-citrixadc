5d5e43488b29f33e73eafc50fa2805892e517c5a
Rohit Myali <rohit.myali@citrix.com>
2023-10-19 06:04:40 +0000

    Updated read operation to make dnsaddrec resource backward compatible
    Signed-off-by: Rohit Myali <rohit.myali@citrix.com>

--- citrixadc/resource_citrixadc_dnsaddrec.go
+++ citrixadc/resource_citrixadc_dnsaddrec.go
@@ -6,6 +6,7 @@ import (
 	"github.com/citrix/adc-nitro-go/service"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 
+	"fmt"
 	"log"
 	"net/url"
 	"strings"
@@ -87,16 +88,32 @@ func createDnsaddrecFunc(d *schema.ResourceData, meta interface{}) error {
 func readDnsaddrecFunc(d *schema.ResourceData, meta interface{}) error {
 	log.Printf("[DEBUG] citrixadc-provider:  In readDnsaddrecFunc")
 	client := meta.(*NetScalerNitroClient).client
-	dnsaddrecId := d.Id()
-	idSlice := strings.SplitN(dnsaddrecId, ",", 2)
+	PrimaryId := d.Id()
+
+	// To make the resource backward compatible, in the prev state file user will have ID with 1 value, but in release v1.27.0 we have updated Id. So here we are changing the code to make it backward compatible
+	// here we are checking for id, if it has 1 elements then we are appending the 2rd attribute to the old Id.
+	oldIdSlice := strings.Split(PrimaryId, ",")
+
+	if len(oldIdSlice) == 1 {
+		if val, ok := d.GetOk("ipaddress"); ok {
+			PrimaryId = PrimaryId + "," + val.(string)
+		}
+		d.SetId(PrimaryId)
+	}
+
+	idSlice := strings.SplitN(PrimaryId, ",", 2)
+	if len(idSlice) != 2 {
+		log.Printf("[DEBUG] citrixadc-provider:  In readDnsaddrecFunc: PrimaryId is not in the correct format")
+		return fmt.Errorf("citrixadc-provider:  In readDnsaddrecFunc: PrimaryId is not in the correct format for dnsaddrec %v", PrimaryId)
+	}
 
 	hostname := idSlice[0]
 	ipaddress := idSlice[1]
 
-	log.Printf("[DEBUG] citrixadc-provider: Reading dnsaddrec state %s", dnsaddrecId)
+	log.Printf("[DEBUG] citrixadc-provider: Reading dnsaddrec state %s", PrimaryId)
 	dataArr, err := client.FindAllResources(service.Dnsaddrec.Type())
 	if err != nil {
-		log.Printf("[WARN] citrixadc-provider: Clearing dnsaddrec state %s", dnsaddrecId)
+		log.Printf("[WARN] citrixadc-provider: Clearing dnsaddrec state %s", PrimaryId)
 		d.SetId("")
 		return nil
 	}
@@ -116,7 +133,7 @@ func readDnsaddrecFunc(d *schema.ResourceData, meta interface{}) error {
 
 	if foundIndex == -1 {
 		log.Printf("[DEBUG] citrixadc-provider: FindResourceArrayWithParams dnsaddrec not found in array")
-		log.Printf("[WARN] citrixadc-provider: Clearing dnsaddrec state %s", dnsaddrecId)
+		log.Printf("[WARN] citrixadc-provider: Clearing dnsaddrec state %s", PrimaryId)
 		d.SetId("")
 		return nil
 	}
