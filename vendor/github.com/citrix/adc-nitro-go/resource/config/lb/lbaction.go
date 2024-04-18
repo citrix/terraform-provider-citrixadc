2303957e48d6b1dff491fbd421c7640e66dd1a82
Rohit Myali <rohit.myali@citrix.com>
2024-04-18 11:36:04 +0530

    Updated vendor directory to fetch the latest adc-nitro-go changes
    Signed-off-by: Rohit Myali <rohit.myali@citrix.com>

---
+++ vendor/github.com/citrix/adc-nitro-go/resource/config/lb/lbaction.go
@@ -0,0 +1,51 @@
+/*
+* Copyright (c) 2024 Citrix Systems, Inc.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+
+package lb
+
+/**
+* Configuration for lb action resource.
+ */
+ type Lbaction struct {
+	/**
+	* Name for the LB action. Must begin with a letter, number, or the underscore character (_), and must contain only letters, numbers, and the hyphen (-), period (.) hash (#), space ( ), at (@), equals (=), colon (:), and underscore characters.
+
+	* The following requirement applies only to the Citrix ADC CLI:
+	* If the name includes one or more spaces, enclose the name in double or single quotation marks (for example, "my lb action" or 'my lb action').
+	 */
+	Name string `json:"name,omitempty"`
+	/**
+	* Type of an LB action. Available settings function as follows:
+	* NOLBACTION - Does not consider LB action in making LB decision.
+	* SELECTIONORDER - services bound to vserver with order specified in value parameter is considerd for lb/gslb decision.
+	Possible values = NOLBACTION, SELECTIONORDER
+	*/
+	Type string `json:"type,omitempty"`
+	/**
+	* The selection order list used during lb/gslb decision. Preference of services during lb/gslb decision is as follows - services corresponding to first order specified in the sequence is considered first, services corresponding to second order specified in the sequence is considered next and so on.
+	* For example, if -value 2 1 3 is specified here and service-1 bound to a vserver with order 1, service-2 bound to a vserver with order 2 and service-3 bound to a vserver with order 3. Then preference of selecting services in LB decision is as follows: service-2, service-1, service-3.
+	* Minimum value = 1
+	 */
+	Value []int `json:"value,omitempty"`
+	/**
+	* Comment. Any type of information about this LB action.
+	 */
+	Comment string `json:"comment,omitempty"`
+	/**
+	* New name for the load balancing virtual server group.
+	 */
+	Newname string `json:"newname,omitempty"`
+}
