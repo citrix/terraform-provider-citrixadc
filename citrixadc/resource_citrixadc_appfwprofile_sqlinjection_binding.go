8005b045b1d8fdfe5cb5078d4c93622dc4a54a23
Rohit Myali <rohit.myali@citrix.com>
2024-08-13 15:08:26 +0530

    Updated delete method with supported query-parameters in url
    Signed-off-by: Rohit Myali <rohit.myali@citrix.com>

--- citrixadc/resource_citrixadc_appfwprofile_sqlinjection_binding.go
+++ citrixadc/resource_citrixadc_appfwprofile_sqlinjection_binding.go
@@ -210,6 +210,16 @@ func deleteAppfwprofile_sqlinjection_bindingFunc(d *schema.ResourceData, meta in
 	args["formactionurl_sql"] = url.QueryEscape(d.Get("formactionurl_sql").(string))
 	args["as_scan_location_sql"] = d.Get("as_scan_location_sql").(string)
 
+	if val, ok := d.GetOk("as_value_type_sql"); ok {
+		args["as_value_type_sql"] = url.QueryEscape(val.(string))
+	}
+	if val, ok := d.GetOk("as_value_expr_sql"); ok {
+		args["as_value_expr_sql"] = url.QueryEscape(val.(string))
+	}
+	if val, ok := d.GetOk("ruletype"); ok {
+		args["ruletype"] = url.QueryEscape(val.(string))
+	}
+
 	err := client.DeleteResourceWithArgsMap(service.Appfwprofile_sqlinjection_binding.Type(), appFwName, args)
 	if err != nil {
 		return err
