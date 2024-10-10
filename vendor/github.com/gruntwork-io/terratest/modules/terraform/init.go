92acd9d78540a3718821606076097c2d86d6ba00
Rohit Myali <rohit.myali@citrix.com>
2024-10-10 06:16:27 +0000

    Updated dependent libraries terratest and go-git
    Signed-off-by: Rohit Myali <rohit.myali@citrix.com>

--- vendor/github.com/gruntwork-io/terratest/modules/terraform/init.go
+++ vendor/github.com/gruntwork-io/terratest/modules/terraform/init.go
@@ -27,6 +27,10 @@ func InitE(t testing.TestingT, options *Options) (string, error) {
 	if options.MigrateState {
 		args = append(args, "-migrate-state", "-force-copy")
 	}
+	// Append no-color option if needed
+	if options.NoColor {
+		args = append(args, "-no-color")
+	}
 
 	args = append(args, FormatTerraformBackendConfigAsArgs(options.BackendConfig)...)
 	args = append(args, FormatTerraformPluginDirAsArgs(options.PluginDir)...)
