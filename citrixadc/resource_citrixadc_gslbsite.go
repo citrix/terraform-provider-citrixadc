c6c1a29169d2512237ef8f84eb68ba2026554b12
lakshmj <lakshman.mj@cloud.com>
2025-07-31 22:21:52 -0700

    Refreshing gslbsite resource with updated attributes.

--- citrixadc/resource_citrixadc_gslbsite.go
+++ citrixadc/resource_citrixadc_gslbsite.go
@@ -1,7 +1,6 @@
 package citrixadc
 
 import (
-	"github.com/citrix/adc-nitro-go/resource/config/gslb"
 	"github.com/citrix/adc-nitro-go/service"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -10,6 +9,89 @@ import (
 	"fmt"
 	"log"
 )
+
+/**
+* Configuration for GSLB site resource.
+ */
+type Gslbsite struct {
+	/**
+	* Name for the GSLB site. Must begin with an ASCII alphanumeric or underscore (_) character, and must contain only ASCII alphanumeric, underscore, hash (#), period (.), space, colon (:), at (@), equals (=), and hyphen (-) characters. Cannot be changed after the virtual server is created.
+		CLI Users: If the name includes one or more spaces, enclose the name in double or single quotation marks (for example, "my gslbsite" or 'my gslbsite').
+	*/
+	Sitename string `json:"sitename,omitempty"`
+	/**
+	* Type of site to create. If the type is not specified, the appliance automatically detects and sets the type on the basis of the IP address being assigned to the site. If the specified site IP address is owned by the appliance (for example, a MIP address or SNIP address), the site is a local site. Otherwise, it is a remote site.
+	 */
+	Sitetype string `json:"sitetype,omitempty"`
+	/**
+	* IP address for the GSLB site. The GSLB site uses this IP address to communicate with other GSLB sites. For a local site, use any IP address that is owned by the appliance (for example, a SNIP or MIP address, or the IP address of the ADNS service).
+	 */
+	Siteipaddress string `json:"siteipaddress,omitempty"`
+	/**
+	* Public IP address for the local site. Required only if the appliance is deployed in a private address space and the site has a public IP address hosted on an external firewall or a NAT device.
+	 */
+	Publicip string `json:"publicip,omitempty"`
+	/**
+	* Exchange metrics with other sites. Metrics are exchanged by using Metric Exchange Protocol (MEP). The appliances in the GSLB setup exchange health information once every second.
+		If you disable metrics exchange, you can use only static load balancing methods (such as round robin, static proximity, or the hash-based methods), and if you disable metrics exchange when a dynamic load balancing method (such as least connection) is in operation, the appliance falls back to round robin. Also, if you disable metrics exchange, you must use a monitor to determine the state of GSLB services. Otherwise, the service is marked as DOWN.
+	*/
+	Metricexchange string `json:"metricexchange,omitempty"`
+	/**
+	* Exchange, with other GSLB sites, network metrics such as round-trip time (RTT), learned from communications with various local DNS (LDNS) servers used by clients. RTT information is used in the dynamic RTT load balancing method, and is exchanged every 5 seconds.
+	 */
+	Nwmetricexchange string `json:"nwmetricexchange,omitempty"`
+	/**
+	* Exchange persistent session entries with other GSLB sites every five seconds.
+	 */
+	Sessionexchange string `json:"sessionexchange,omitempty"`
+	/**
+	* Specify the conditions under which the GSLB service must be monitored by a monitor, if one is bound. Available settings function as follows:
+		* ALWAYS - Monitor the GSLB service at all times.
+		* MEPDOWN - Monitor the GSLB service only when the exchange of metrics through the Metrics Exchange Protocol (MEP) is disabled.
+		MEPDOWN_SVCDOWN - Monitor the service in either of the following situations:
+		* The exchange of metrics through MEP is disabled.
+		* The exchange of metrics through MEP is enabled but the status of the service, learned through metrics exchange, is DOWN.
+	*/
+	Triggermonitor string `json:"triggermonitor,omitempty"`
+	/**
+	* Parent site of the GSLB site, in a parent-child topology.
+	 */
+	Parentsite string `json:"parentsite,omitempty"`
+	/**
+	* Cluster IP address. Specify this parameter to connect to the remote cluster site for GSLB auto-sync. Note: The cluster IP address is defined when creating the cluster.
+	 */
+	Clip string `json:"clip,omitempty"`
+	/**
+	* IP address to be used to globally access the remote cluster when it is deployed behind a NAT. It can be same as the normal cluster IP address.
+	 */
+	Publicclip string `json:"publicclip,omitempty"`
+	/**
+	* The naptr replacement suffix configured here will be used to construct the naptr replacement field in NAPTR record.
+	 */
+	Naptrreplacementsuffix string `json:"naptrreplacementsuffix,omitempty"`
+	/**
+	* The list of backup gslb sites configured in preferred order. Need to be parent gsb sites.
+	 */
+	Backupparentlist []string `json:"backupparentlist,omitempty"`
+	/**
+	* Password to be used for mep communication between gslb site nodes.
+	 */
+	Sitepassword string `json:"sitepassword,omitempty"`
+	/**
+	* New name for the GSLB site.
+	 */
+	Newname string `json:"newname,omitempty"`
+
+	//------- Read only Parameter ---------;
+
+	Status               string `json:"status,omitempty"`
+	Persistencemepstatus string `json:"persistencemepstatus,omitempty"`
+	Version              string `json:"version,omitempty"`
+	Curbackupparentip    string `json:"curbackupparentip,omitempty"`
+	Sitestate            string `json:"sitestate,omitempty"`
+	Oldname              string `json:"oldname,omitempty"`
+	Nextgenapiresource   string `json:"_nextgenapiresource,omitempty"`
+}
 
 func resourceCitrixAdcGslbsite() *schema.Resource {
 	return &schema.Resource{
@@ -47,7 +129,6 @@ func resourceCitrixAdcGslbsite() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
-				ForceNew: true,
 			},
 			"publicclip": {
 				Type:     schema.TypeString,
@@ -59,7 +140,6 @@ func resourceCitrixAdcGslbsite() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
-				ForceNew: true,
 			},
 			"sessionexchange": {
 				Type:     schema.TypeString,
@@ -70,7 +150,6 @@ func resourceCitrixAdcGslbsite() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
-				ForceNew: true,
 			},
 			"sitename": {
 				Type:     schema.TypeString,
@@ -84,6 +163,22 @@ func resourceCitrixAdcGslbsite() *schema.Resource {
 				ForceNew: true,
 			},
 			"triggermonitor": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"backupparentlist": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"sitepassword": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"newname": {
 				Type:     schema.TypeString,
 				Optional: true,
 				Computed: true,
@@ -102,7 +197,7 @@ func createGslbsiteFunc(d *schema.ResourceData, meta interface{}) error {
 		gslbsiteName = resource.PrefixedUniqueId("tf-gslbsite-")
 		d.Set("sitename", gslbsiteName)
 	}
-	gslbsite := gslb.Gslbsite{
+	gslbsite := Gslbsite{
 		Clip:                   d.Get("clip").(string),
 		Metricexchange:         d.Get("metricexchange").(string),
 		Naptrreplacementsuffix: d.Get("naptrreplacementsuffix").(string),
@@ -115,8 +210,11 @@ func createGslbsiteFunc(d *schema.ResourceData, meta interface{}) error {
 		Sitename:               d.Get("sitename").(string),
 		Sitetype:               d.Get("sitetype").(string),
 		Triggermonitor:         d.Get("triggermonitor").(string),
+		Sitepassword:           d.Get("sitepassword").(string),
 	}
-
+	if listVal, ok := d.Get("backupparentlist").([]interface{}); ok {
+		gslbsite.Backupparentlist = toStringList(listVal)
+	}
 	_, err := client.AddResource(service.Gslbsite.Type(), gslbsiteName, &gslbsite)
 	if err != nil {
 		return err
@@ -153,9 +251,23 @@ func readGslbsiteFunc(d *schema.ResourceData, meta interface{}) error {
 	d.Set("publicip", data["publicip"])
 	d.Set("sessionexchange", data["sessionexchange"])
 	d.Set("siteipaddress", data["siteipaddress"])
-	d.Set("sitename", data["sitename"])
 	d.Set("sitetype", data["sitetype"])
 	d.Set("triggermonitor", data["triggermonitor"])
+	if val, ok := data["backupparentlist"]; ok {
+		if list, ok := val.([]interface{}); ok {
+			d.Set("backupparentlist", toStringList(list))
+		}
+	} else {
+		d.Set("backupparentlist", nil)
+	}
+	d.Set("sitepassword", d.Get("sitepassword").(string))
+	d.Set("status", data["status"])
+	d.Set("persistencemepstatus", data["persistencemepstatus"])
+	d.Set("version", data["version"])
+	d.Set("curbackupparentip", data["curbackupparentip"])
+	d.Set("sitestate", data["sitestate"])
+	d.Set("oldname", data["oldname"])
+	d.Set("nextgenapiresource", data["_nextgenapiresource"])
 
 	return nil
 
@@ -166,15 +278,11 @@ func updateGslbsiteFunc(d *schema.ResourceData, meta interface{}) error {
 	client := meta.(*NetScalerNitroClient).client
 	gslbsiteName := d.Get("sitename").(string)
 
-	gslbsite := gslb.Gslbsite{
+	gslbsite := Gslbsite{
 		Sitename: d.Get("sitename").(string),
 	}
+	hasRename := false
 	hasChange := false
-	if d.HasChange("clip") {
-		log.Printf("[DEBUG]  netscaler-provider: Clip has changed for gslbsite %s, starting update", gslbsiteName)
-		gslbsite.Clip = d.Get("clip").(string)
-		hasChange = true
-	}
 	if d.HasChange("metricexchange") {
 		log.Printf("[DEBUG]  netscaler-provider: Metricexchange has changed for gslbsite %s, starting update", gslbsiteName)
 		gslbsite.Metricexchange = d.Get("metricexchange").(string)
@@ -193,11 +301,6 @@ func updateGslbsiteFunc(d *schema.ResourceData, meta interface{}) error {
 	if d.HasChange("parentsite") {
 		log.Printf("[DEBUG]  netscaler-provider: Parentsite has changed for gslbsite %s, starting update", gslbsiteName)
 		gslbsite.Parentsite = d.Get("parentsite").(string)
-		hasChange = true
-	}
-	if d.HasChange("publicclip") {
-		log.Printf("[DEBUG]  netscaler-provider: Publicclip has changed for gslbsite %s, starting update", gslbsiteName)
-		gslbsite.Publicclip = d.Get("publicclip").(string)
 		hasChange = true
 	}
 	if d.HasChange("publicip") {
@@ -220,23 +323,37 @@ func updateGslbsiteFunc(d *schema.ResourceData, meta interface{}) error {
 		gslbsite.Sitename = d.Get("sitename").(string)
 		hasChange = true
 	}
-	if d.HasChange("sitetype") {
-		log.Printf("[DEBUG]  netscaler-provider: Sitetype has changed for gslbsite %s, starting update", gslbsiteName)
-		gslbsite.Sitetype = d.Get("sitetype").(string)
-		hasChange = true
-	}
 	if d.HasChange("triggermonitor") {
 		log.Printf("[DEBUG]  netscaler-provider: Triggermonitor has changed for gslbsite %s, starting update", gslbsiteName)
 		gslbsite.Triggermonitor = d.Get("triggermonitor").(string)
 		hasChange = true
 	}
+	if d.HasChange("backupparentlist") {
+		log.Printf("[DEBUG]  netscaler-provider: Backupparentlist has changed for gslbsite %s, starting update", gslbsiteName)
+		gslbsite.Backupparentlist = toStringList(d.Get("backupparentlist").([]interface{}))
+		hasChange = true
+	}
+	if d.HasChange("newname") {
+		log.Printf("[DEBUG]  netscaler-provider: Newname has changed for gslbsite %s, starting update", gslbsiteName)
+		gslbsite.Newname = d.Get("newname").(string)
+		hasRename = true
+	}
 
 	if hasChange {
 		_, err := client.UpdateResource(service.Gslbsite.Type(), gslbsiteName, &gslbsite)
 		if err != nil {
-			return fmt.Errorf("Error updating gslbsite %s", gslbsiteName)
+			return fmt.Errorf("error updating gslbsite %s", gslbsiteName)
 		}
 	}
+
+	if hasRename {
+		err := client.ActOnResource(service.Gslbsite.Type(), &gslbsite, "rename")
+		if err != nil {
+			return fmt.Errorf("error renaming gslbsite %s", gslbsiteName)
+		}
+		d.SetId(gslbsite.Newname)
+	}
+
 	return readGslbsiteFunc(d, meta)
 }
 
