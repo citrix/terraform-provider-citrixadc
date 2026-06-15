3ccf66f15a07ae5d83ecc0ccb2ac678eaa35d7fc
lakshmj <lakshman.mj@citrix.com>
2026-06-15 01:41:16 -0700

    Migrate lbvserver_analyticsprofile_binding to Plugin Framework (wave 9)

--- citrixadc_framework/acctests/lbvserver_analyticsprofile_binding_test.go
+++ citrixadc_framework/acctests/lbvserver_analyticsprofile_binding_test.go
@@ -17,10 +17,10 @@ package citrixadc
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/citrix/adc-nitro-go/service"
+	"github.com/citrix/terraform-provider-citrixadc/citrixadc_framework/utils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
@@ -82,10 +82,12 @@ func testAccCheckLbvserver_analyticsprofile_bindingExist(n string, id *string) r
 
 		bindingId := rs.Primary.ID
 
-		idSlice := strings.SplitN(bindingId, ",", 2)
-
-		name := idSlice[0]
-		analyticsprofile := idSlice[1]
+		idMap, _, err := utils.ParseIdString(bindingId, []string{"name", "analyticsprofile"}, nil)
+		if err != nil {
+			return err
+		}
+		name := idMap["name"]
+		analyticsprofile := idMap["analyticsprofile"]
 
 		findParams := service.FindParams{
 			ResourceType:             "lbvserver_analyticsprofile_binding",
