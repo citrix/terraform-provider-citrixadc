aad46b9f3afbbe9e1279da911f68329b21407bd0
lakshmj <lakshman.mj@citrix.com>
2026-06-15 06:52:34 -0700

    Migrate vpnvserver_vpnnexthopserver_binding to Plugin Framework (wave 21)

--- citrixadc_framework/acctests/vpnvserver_vpnnexthopserver_binding_test.go
+++ citrixadc_framework/acctests/vpnvserver_vpnnexthopserver_binding_test.go
@@ -17,10 +17,10 @@ package citrixadc
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/citrix/adc-nitro-go/service"
+	"github.com/citrix/terraform-provider-citrixadc/citrixadc_framework/utils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
@@ -131,10 +131,12 @@ func testAccCheckVpnvserver_vpnnexthopserver_bindingExist(n string, id *string) 
 
 		bindingId := rs.Primary.ID
 
-		idSlice := strings.SplitN(bindingId, ",", 2)
-
-		name := idSlice[0]
-		nexthopserver := idSlice[1]
+		idMap, _, err := utils.ParseIdString(bindingId, []string{"name", "nexthopserver"}, nil)
+		if err != nil {
+			return err
+		}
+		name := idMap["name"]
+		nexthopserver := idMap["nexthopserver"]
 
 		findParams := service.FindParams{
 			ResourceType:             "vpnvserver_vpnnexthopserver_binding",
@@ -173,13 +175,12 @@ func testAccCheckVpnvserver_vpnnexthopserver_bindingNotExist(n string, id string
 			return fmt.Errorf("Failed to get test client: %v", err)
 		}
 
-		if !strings.Contains(id, ",") {
-			return fmt.Errorf("Invalid id string %v. The id string must contain a comma.", id)
+		idMap, _, err := utils.ParseIdString(id, []string{"name", "nexthopserver"}, nil)
+		if err != nil {
+			return err
 		}
-		idSlice := strings.SplitN(id, ",", 2)
-
-		name := idSlice[0]
-		nexthopserver := idSlice[1]
+		name := idMap["name"]
+		nexthopserver := idMap["nexthopserver"]
 
 		findParams := service.FindParams{
 			ResourceType:             "vpnvserver_vpnnexthopserver_binding",
