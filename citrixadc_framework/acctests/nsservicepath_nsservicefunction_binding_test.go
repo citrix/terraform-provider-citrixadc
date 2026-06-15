b5a4af9a4be03280fb501e194c80336cc108a2f5
lakshmj <lakshman.mj@citrix.com>
2026-06-15 02:18:47 -0700

    Migrate nsservicepath_nsservicefunction_binding to Plugin Framework (wave 11)

--- citrixadc_framework/acctests/nsservicepath_nsservicefunction_binding_test.go
+++ citrixadc_framework/acctests/nsservicepath_nsservicefunction_binding_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"github.com/citrix/adc-nitro-go/service"
+	"github.com/citrix/terraform-provider-citrixadc/citrixadc_framework/utils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
@@ -134,10 +135,12 @@ func testAccCheckNsservicepath_nsservicefunction_bindingExist(n string, id *stri
 
 		bindingId := rs.Primary.ID
 
-		idSlice := strings.SplitN(bindingId, ",", 2)
-
-		servicepathname := idSlice[0]
-		servicefunction := idSlice[1]
+		idMap, _, err := utils.ParseIdString(bindingId, []string{"servicepathname", "servicefunction"}, nil)
+		if err != nil {
+			return err
+		}
+		servicepathname := idMap["servicepathname"]
+		servicefunction := idMap["servicefunction"]
 
 		findParams := service.FindParams{
 			ResourceType:             "nsservicepath_nsservicefunction_binding",
