257b3dc66f6da466b0e05ccfad931e586ed0d444
lakshmj <lakshman.mj@citrix.com>
2026-06-15 02:39:52 -0700

    Migrate rnat_nsip_binding to Plugin Framework (wave 12)

--- citrixadc_framework/acctests/rnat_nsip_binding_test.go
+++ citrixadc_framework/acctests/rnat_nsip_binding_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"github.com/citrix/adc-nitro-go/service"
+	"github.com/citrix/terraform-provider-citrixadc/citrixadc_framework/utils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
@@ -116,10 +117,12 @@ func testAccCheckRnat_nsip_bindingExist(n string, id *string) resource.TestCheck
 
 		bindingId := rs.Primary.ID
 
-		idSlice := strings.SplitN(bindingId, ",", 2)
-
-		name := idSlice[0]
-		natip := idSlice[1]
+		idMap, _, err := utils.ParseIdString(bindingId, []string{"name", "natip"}, nil)
+		if err != nil {
+			return fmt.Errorf("Error parsing ID %s: %v", bindingId, err)
+		}
+		name := idMap["name"]
+		natip := idMap["natip"]
 
 		findParams := service.FindParams{
 			ResourceType:             "rnat_nsip_binding",
