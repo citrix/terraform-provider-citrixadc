e53b09b7c02ec82542789dd9d4179fc843ab0e11
lakshmj <lakshman.mj@citrix.com>
2026-06-15 06:24:33 -0700

    Migrate vpnvserver_authenticationwebauthpolicy_binding to Plugin Framework (wave 19)

--- citrixadc_framework/acctests/vpnvserver_authenticationwebauthpolicy_binding_test.go
+++ citrixadc_framework/acctests/vpnvserver_authenticationwebauthpolicy_binding_test.go
@@ -17,10 +17,10 @@ package citrixadc
 
 import (
 	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/citrix/adc-nitro-go/service"
+	"github.com/citrix/terraform-provider-citrixadc/citrixadc_framework/utils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
@@ -127,10 +127,12 @@ func testAccCheckVpnvserver_authenticationwebauthpolicy_bindingExist(n string, i
 
 		bindingId := rs.Primary.ID
 
-		idSlice := strings.SplitN(bindingId, ",", 2)
-
-		name := idSlice[0]
-		policy := idSlice[1]
+		idMap, _, err := utils.ParseIdString(bindingId, []string{"name", "policy"}, nil)
+		if err != nil {
+			return fmt.Errorf("Error parsing ID %v: %v", bindingId, err)
+		}
+		name := idMap["name"]
+		policy := idMap["policy"]
 
 		findParams := service.FindParams{
 			ResourceType:             "vpnvserver_authenticationwebauthpolicy_binding",
@@ -169,13 +171,12 @@ func testAccCheckVpnvserver_authenticationwebauthpolicy_bindingNotExist(n string
 			return fmt.Errorf("Failed to get test client: %v", err)
 		}
 
-		if !strings.Contains(id, ",") {
-			return fmt.Errorf("Invalid id string %v. The id string must contain a comma.", id)
+		idMap, _, err := utils.ParseIdString(id, []string{"name", "policy"}, nil)
+		if err != nil {
+			return fmt.Errorf("Error parsing ID %v: %v", id, err)
 		}
-		idSlice := strings.SplitN(id, ",", 2)
-
-		name := idSlice[0]
-		policy := idSlice[1]
+		name := idMap["name"]
+		policy := idMap["policy"]
 
 		findParams := service.FindParams{
 			ResourceType:             "vpnvserver_authenticationwebauthpolicy_binding",
