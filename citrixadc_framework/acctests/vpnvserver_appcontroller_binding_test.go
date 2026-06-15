118e29d871911d1f294297f71d08586bfa2f17e4
lakshmj <lakshman.mj@citrix.com>
2026-06-15 06:03:06 -0700

    Migrate vpnvserver_appcontroller_binding to Plugin Framework (wave 18)

--- citrixadc_framework/acctests/vpnvserver_appcontroller_binding_test.go
+++ citrixadc_framework/acctests/vpnvserver_appcontroller_binding_test.go
@@ -21,6 +21,7 @@ import (
 	"testing"
 
 	"github.com/citrix/adc-nitro-go/service"
+	"github.com/citrix/terraform-provider-citrixadc/citrixadc_framework/utils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
@@ -100,10 +101,12 @@ func testAccCheckVpnvserver_appcontroller_bindingExist(n string, id *string) res
 
 		bindingId := rs.Primary.ID
 
-		idSlice := strings.SplitN(bindingId, ",", 2)
-
-		name := idSlice[0]
-		appcontroller := idSlice[1]
+		idMap, _, err := utils.ParseIdString(bindingId, []string{"name", "appcontroller"}, nil)
+		if err != nil {
+			return fmt.Errorf("Error parsing ID %s: %v", bindingId, err)
+		}
+		name := idMap["name"]
+		appcontroller := idMap["appcontroller"]
 
 		findParams := service.FindParams{
 			ResourceType:             "vpnvserver_appcontroller_binding",
